a640f117f4fc56498f9b2d9b6d9275c4457a2188
minayao <minayao9@gmail.com>
2016-10-30 03:00:54 -0700

    FAQ page, fixed footer opening bug in mobile, switched back to a normal footer for now.

---
+++ src/CollAction/wwwroot/js/faq.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $('.collapse').on('show.bs.collapse', function () {
+        $(this).parent().find(".fa-angle-down").removeClass("fa-angle-down").addClass("fa-angle-up");
+    });
+    $('.collapse').on('hide.bs.collapse', function () {
+        $(this).parent().find(".fa-angle-up").removeClass("fa-angle-up").addClass("fa-angle-down");
+    });
+
+});
