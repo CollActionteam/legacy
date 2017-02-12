6ac90e3b06aa279efba59124de554535991356ac
Stewart Matheson <stew@rtmatheson.com>
2017-02-12 11:30:28 +1100

    Fixed issues with old grid system by adding a newer one Started setting up project page for new styles

--- src/CollAction/frontend/app/index.ts
+++ src/CollAction/frontend/app/index.ts
@@ -1,7 +1,6 @@
+import "./global/bootstrap.css"
 import "./project/style.scss"
 import "./forms/style.scss"
 import "./header/style.scss"
 import "./footer/style.scss"
 import "./global/body.scss"
-
-import "bootstrap-grid"
