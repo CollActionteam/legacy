e30530d093652ad6c3a4fb7ad53218c5e235c1a2
Stewart Matheson <stew@rtmatheson.com>
2017-03-20 20:50:53 +1100

    Start project search page. Added events for changes to select states

--- CollAction/Frontend/app/project/DropDown.tsx
+++ CollAction/Frontend/app/project/DropDown.tsx
@@ -1,31 +1,51 @@
 import * as React from "react";
 
-const Options = (list) => <div className="drop-down-options"></div>;
+const Options = ({ list, onSelected }) => {
+  return (
+    <div className="drop-down-options">
+      <ul>
+        {list.map(item => <li key={item} onClick={() => onSelected(item)}>{item}</li>)}
+      </ul>
+    </div>
+  );
+};
 
 interface IDropdownProps {
   options: string[];
+  onChange: (selected: string) => void;
 }
 
 interface IDropdownState {
   open: boolean;
+  currentlySelected: string;
 }
 
 export default class DropDown extends React.Component<IDropdownProps, IDropdownState> {
   constructor (props) {
     super(props);
-    this.state = { open: false };
+    this.state = { open: false, currentlySelected: this.props.options[0] };
   }
 
   onClick(...args) {
     this.setState({open: !this.state.open});
   }
 
+  onSelected (option: string) {
+    this.setState({
+      currentlySelected: option
+    });
+
+    if (this.state.currentlySelected !== option) {
+      this.props.onChange(this.state.currentlySelected);
+    }
+  }
+
   render () {
     return (
       <div onClick={() => this.onClick()} className="project-filter-drop-down">
-        {this.props.options[0]}
+        {this.state.currentlySelected}
         {this.state.open ? <i className="fa fa-chevron-up" /> : <i className="fa fa-chevron-down" />}
-        {this.state.open ? <Options list={this.props.options} /> : null}
+        {this.state.open ? <Options list={this.props.options} onSelected={(selection) => this.onSelected(selection)} /> : null}
       </div>
     );
   }
--- CollAction/Frontend/app/project/ProjectFilter.tsx
+++ CollAction/Frontend/app/project/ProjectFilter.tsx
@@ -2,12 +2,21 @@ import * as React from "react";
 import DropDown from "./DropDown";
 
 const filterList = [ "all" ];
-const statusList = [ "open" ];
+const statusList = [ "open", "closed", "funded" ];
 const locations = [ "earth" ];
+
+function onFilterChanged () {
+  console.log('onChange');
+}
 
 export default () => (
   <div id="project-filter">
-    Show me <DropDown options={filterList} /> projects sorted on <DropDown options={locations}/>  <br />
-    which are <DropDown options={statusList} />
+    Show me
+    <DropDown onChange={onFilterChanged} options={filterList} />
+    projects sorted on
+    <DropDown onChange={onFilterChanged} options={locations}/>
+    <br />
+    which are
+    <DropDown onChange={onFilterChanged} options={statusList} />
   </div>
 );
