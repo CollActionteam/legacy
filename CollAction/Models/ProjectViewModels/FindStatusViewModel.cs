8a01d79e64c376560f1f6f8c1dcb3d86b6dfd506
minayao <minayao9@gmail.com>
2017-04-28 16:38:53 -0700

    CA-188 status endpoint, update project endpoint with status

---
+++ CollAction/Models/ProjectViewModels/FindStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollAction.Models
+{
+    public class FindStatusViewModel
+    {
+        public string Id { get; set; }
+
+        public string Status { get; set; }
+    }
+}
