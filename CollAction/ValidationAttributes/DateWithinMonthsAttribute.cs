9e0d923b625d29ec3a199588b84fa0f2a3d93b18
MarcB <MarcBeermann@hotmail.com>
2017-05-04 13:25:39 +1000

    Start and End date validation CA_207 and CA_197: - added a validation attribute 'DateWithinMonths' to ensure that the startdate is at least within a year of today's date. - added a validation attribute 'DateWithinMonths' to ensure that the enddate is within a year of the start date. - updated CreatProject/EditProject/ManageProjectViewModels accordingly. - moved the YouTubeLinkAttribute.cs file up a level. - fixed broken links to edit project from the Manage index page.

---
+++ CollAction/ValidationAttributes/DateWithinMonthsAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CollAction.ValidationAttributes
+{
+    public class DateWithinMonthsAttribute : ValidationAttribute
+    {
+        private readonly int _months;
+
+        public String OfDateProperty { get; set; }
+
+        public DateWithinMonthsAttribute(int months)
+        {
+            _months = months;
+        }
+
+        /// <summary>
+        ///     If no {OfDateProperty} is specified, then this date is valid if it falls withing {months} of todays date (not including today).
+        /// </summary>
+        /// <param months="number of months within which this date must lie"></param>
+        /// <param OfDateProperty="a DateTime property in this model"></param>
+        /// <returns></returns>
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value == null) return ValidationResult.Success;
+
+            DateTime startDate = DateTime.UtcNow;
+            if (OfDateProperty != null)
+            {
+                Object instance = context.ObjectInstance;
+                PropertyInfo propertyInfo = instance.GetType().GetProperty(OfDateProperty);
+                startDate = (DateTime)propertyInfo.GetValue(instance, null);
+            }
+
+            DateTime endDate = startDate.AddMonths(_months);
+
+            DateTime checkDate = (DateTime)value;
+            if (checkDate <= startDate || checkDate > endDate)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
