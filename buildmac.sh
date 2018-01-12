8545364020b775cd1200f754ac15e0bc29260e3c
Luuk Boschker <luuk.boschker@gmail.com>
2018-01-12 15:35:23 +0100

    Publish also builds stuff

--- buildmac.sh
+++ buildmac.sh
@@ -1,5 +1,4 @@
 export NODE_ENV="production"
 dotnet restore CollAction
-dotnet build CollAction -c Release
 dotnet publish CollAction -c Release -o bin/netcoreapp1.1/publish
 docker build -t $1 -t collaction CollAction
