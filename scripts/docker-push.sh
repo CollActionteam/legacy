c710a53d262177ecfb0ed43b5a6c3b8858e3fbb8
Luuk Boschker <luuk.boschker@gmail.com>
2018-01-14 16:41:18 +0100

    Trying to get auto-deploy to work (step 1)

---
+++ scripts/docker-push.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+if [ -z "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" == "true" ]; then 
+    # Don't push if it's a pull request
+    echo "Skipping deploy because it's a pull request";
+    exit 0;
+fi
+
+if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+    # Don't push if it's not the correct branch
+    echo "Skipping deploy because branch is not '$DEPLOY_BRANCH'"
+    exit 0;
+fi
+
+# Build and push
+echo "Building $DOCKER_REPO"
+docker build -t $DOCKER_REPO CollAction
+
+echo "Pushing $DOCKER_REPO"
+docker login -p=$DOCKER_PASSWORD -u=$DOCKER_USERNAME
+docker push $DOCKER_REPO
+
+echo "Pushed $DOCKER_REPO"
