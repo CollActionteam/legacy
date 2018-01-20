a4130aea21e8701e85d5dc942666064b9c91a08b
Marc Beermann <marcbeermann@outlook.com>
2018-01-20 17:07:32 +1100

    CA-351-Friesland-Login - Changed green text and buttons to blue. - Translated texts to Dutch.

--- CollAction/Models/AccountViewModels/LoginViewModel.cs
+++ CollAction/Models/AccountViewModels/LoginViewModel.cs
@@ -8,15 +8,20 @@ namespace CollAction.Models.AccountViewModels
 {
     public class LoginViewModel
     {
+        // Email
         [Required]
         [EmailAddress]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
+        // Password
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        // Remember me?
+        [Display(Name = "Onthoud gegevens?")]
         public bool RememberMe { get; set; }
     }
 }
