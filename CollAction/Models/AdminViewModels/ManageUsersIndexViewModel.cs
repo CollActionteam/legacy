39ea9f7a0296d58838b38e257281010e92d0220f
logophobia <>
2018-02-15 06:11:43 +0100

    CA-365

---
+++ CollAction/Models/AdminViewModels/ManageUsersIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CollAction.Models.AdminViewModels
+{
+    public class ManageUsersIndexViewModel
+    {
+        public List<ApplicationUser> Users { get; set; }
+        public int CurrentPage { get; set; }
+        public int NextPage
+            => CurrentPage + 1;
+        public int PreviousPage
+            => CurrentPage - 1;
+        public int NumberPages { get; internal set; }
+    }
+}
