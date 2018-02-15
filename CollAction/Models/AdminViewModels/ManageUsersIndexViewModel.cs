368474b3d32d94811717d56a804109a45277fcf0
logophobia <>
2018-02-15 06:18:19 +0100

    Refine pagination a bit, make it a bit more clear

--- CollAction/Models/AdminViewModels/ManageUsersIndexViewModel.cs
+++ CollAction/Models/AdminViewModels/ManageUsersIndexViewModel.cs
@@ -5,11 +5,21 @@ namespace CollAction.Models.AdminViewModels
     public class ManageUsersIndexViewModel
     {
         public List<ApplicationUser> Users { get; set; }
+
         public int CurrentPage { get; set; }
+
+        public int NumberPages { get; set; }
+
         public int NextPage
             => CurrentPage + 1;
+
+        public bool HasNext
+            => CurrentPage < NumberPages - 1;
+
+        public bool HasPrevious
+            => CurrentPage > 0;
+
         public int PreviousPage
             => CurrentPage - 1;
-        public int NumberPages { get; internal set; }
     }
 }
