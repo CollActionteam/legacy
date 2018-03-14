2dacf6bb375258b0b42a569610e635d79a556553
Abhinay <abhinay@outlook.com>
2018-03-14 19:13:44 -0400

    Updated Admin and Project Contollers to append 23:59:59 to user entered date, added FAQ

--- CollAction/Controllers/ProjectsController.cs
+++ CollAction/Controllers/ProjectsController.cs
@@ -140,7 +140,7 @@ namespace CollAction.Controllers
                 LocationId = model.LocationId,
                 Target = model.Target,
                 Start = model.Start,
-                End = model.End,
+                End = model.End.AddHours(23).AddMinutes(59).AddSeconds(59),
                 BannerImage = null
             };
 
