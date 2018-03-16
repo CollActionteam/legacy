5aba069928d62d2f333b64d92d38c4ff84514d86
Luuk Boschker <luuk.boschker@gmail.com>
2018-03-16 15:12:22 +0100

    Fixed travis and mac docker builds

--- buildmac.sh
+++ buildmac.sh
@@ -1,4 +1,4 @@
 export NODE_ENV=Production
 dotnet restore CollAction
-dotnet publish CollAction -c Release -o bin/netcoreapp1.1/publish
+dotnet publish CollAction -c Release -o bin/netcoreapp/publish
 docker build -t $1 -t collaction CollAction
