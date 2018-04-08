f85eb420b346a0bb4c8ad99c8e5da7c9e480371b
Abhinay <abhinay@outlook.com>
2018-04-08 17:39:13 -0400

    added new migration for change to update friesland repo

---
+++ CollAction/Migrations/20180408213227_UpdateEndDatesFriesland.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CollAction.Migrations
+{
+    public partial class UpdateEndDatesFriesland : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE public.\"Projects\" SET \"End\" = \"End\" + interval '23 hours' + interval '59 minutes' + interval '59 seconds';");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE public.\"Projects\" SET \"End\" = \"End\" - interval '23 hours' - interval '59 minutes' - interval '59 seconds';");
+
+        }
+    }
+}
