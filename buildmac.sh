d37b4d41c21f6892fec1c11e3d4338491a4e6d6b
Luuk Boschker <luuk.boschker@gmail.com>
2018-04-15 21:47:04 +0200

    Build-script for macs

---
+++ buildmac.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export NODE_ENV=production
+dotnet restore CollAction
+dotnet build CollAction -c Release
+dotnet publish CollAction -c Release -o bin/netcoreapp/publish
+docker build -t $1 -t collaction CollAction
