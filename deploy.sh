6d7b56cf3d2a35cc5a25ded5ad95eef7c2e157b4
Luuk Boschker <luuk.boschker@gmail.com>
2018-06-16 15:23:04 +0200

    Updated deploy script help message

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
-   echo Usage: $0 PR-number AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+   echo "Usage: $0 image_tag AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY"
+   echo
+   echo "For example: $0 CA-401 key secret"
+   echo "Key and secret can be found on https://my.1password.com/vaults/wnyzxunff2daamk5rkdovq7lly/allitems/uzcgzv4z4e55w36wyyvjfer5lq"
+   echo
    exit -1;
 fi
 
