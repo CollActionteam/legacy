8c31a35b975b590e26c84f9f8b43fc207d544432
Luuk Boschker <luuk.boschker@gmail.com>
2018-06-16 15:16:47 +0200

    Clearer message if it's not a proper branch name

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -9,7 +9,7 @@ shopt -s nocasematch
 CA_REGEX='(CA-[[:digit:]]+)'
 
 if ! [[ "$TRAVIS_PULL_REQUEST_BRANCH" =~ $CA_REGEX ]]; then
-	echo "Skip puashing to ECR because branch $TRAVIS_PULL_REQUEST_BRANCH doesn't contain a CA number."
+	echo "Skip pushing to ECR because branch '$TRAVIS_PULL_REQUEST_BRANCH' doesn't contain a task number in the format 'CA-n'."
     exit 0;
 fi
 
