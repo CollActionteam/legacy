b385731c35da79b4777ccc34227e49e5f34ad5c5
Luuk Boschker <luuk.boschker@gmail.com>
2018-06-22 18:46:59 +0200

    Fixed compiler warnings

--- CollAction/Controllers/ProjectsController.cs
+++ CollAction/Controllers/ProjectsController.cs
@@ -153,7 +153,7 @@ namespace CollAction.Controllers
 
             // Save project related items (now that we've got a project id
             project.SetDescriptionVideoLink(_context, model.DescriptionVideoLink);
-            project.SetTags(_context, model.Hashtag?.Split(';') ?? new string[0]);
+            await project.SetTags(_context, model.Hashtag?.Split(';') ?? new string[0]);
 
             await _context.SaveChangesAsync();
 
