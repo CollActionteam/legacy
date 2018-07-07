8d1c46c9c1d441cdc9ecfbaf8db0234c365d053d
logophobia <>
2018-07-07 18:40:17 +0200

    Add application insights

--- CollAction/Startup.cs
+++ CollAction/Startup.cs
@@ -108,6 +108,7 @@ namespace CollAction
                 options.MailChimpKey = Configuration["MailChimpKey"];
                 options.MailChimpNewsletterListId = Configuration["MailChimpNewsletterListId"];
             });
+            services.AddApplicationInsightsTelemetry(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
