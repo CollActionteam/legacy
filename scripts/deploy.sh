95a6f71e396b12be7828a298765b8b28cab0259a
Luuk Boschker <luuk.boschker@gmail.com>
2018-07-13 15:48:24 +0200

    Auto-deploy master and Friesland branches to ECS

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    ECS_SERVICENAME="CollActionAcceptance"
+elif [[ "$TRAVIS_BRANCH" == "Friesland" ]]; then
+    ECS_SERVICENAME="CollActionFreonenAcceptance"
+else
+    echo "Not deploying branch '$TRAVIS_BRANCH' because it's not master or Friesland"
+    exit 0
+fi
+
+echo "Deploying '$TRAVIS_BRANCH' to $ECS_SERVICENAME"
+docker run -it --rm -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" silintl/ecs-deploy -r eu-central-1 -n $ECS_SERVICENAME -i 156764677614.dkr.ecr.eu-central-1.amazonaws.com/collaction:$TRAVIS_BRANCH -c CollActionTest
