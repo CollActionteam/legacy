65494214b20df72e607e336068102f2f7a875c76
dominicus <dominik.guz11@gmail.com>
2018-07-29 13:57:29 +0200

    EDIT: mobile menu include account navigation when logged in

--- CollAction/Frontend/app/global/MobileNavDraw.tsx
+++ CollAction/Frontend/app/global/MobileNavDraw.tsx
@@ -37,6 +37,10 @@ export default class MobileNavDraw extends React.Component<IMobileNavDrawProps, 
             <li><a href="/start">Start Project</a></li>
             <li><a href="/about">About</a></li>
             {this.props.loginText == "Logout" ?
+                (<li><a href="/manage">Account</a></li>) :
+                (<li></li>)
+            }
+            {this.props.loginText == "Logout" ?
                 (<li><button type="button" onClick={() => document.getElementById("logOutBtn").click()}>Logout</button></li>) :
                 (<li><a href="/account/login">Login</a></li>)
             }
