1d047626aebb0147c1f71f5f5f06389d07bc1a55
dominicus <dominik.guz11@gmail.com>
2018-07-30 21:47:36 +0200

    EDIT: shorten logic

--- CollAction/Models/ProjectViewModels/DisplayProjectViewModel.cs
+++ CollAction/Models/ProjectViewModels/DisplayProjectViewModel.cs
@@ -86,10 +86,8 @@ namespace CollAction.Models
                     return $"{remaining.Weeks()} weeks";
                 else if (remaining.Days > 1)
                     return $"{remaining.Days} days";
-                else if (remaining.TotalHours >= 24)
-                    return $"{(int)remaining.TotalHours} hours";
                 else if (remaining.Hours > 1)
-                    return $"{remaining.Hours} hours";
+                    return $"{(int)remaining.TotalHours} hours";
                 else if (remaining.Minutes > 0)
                     return $"{remaining.Minutes} minutes";
                 else
