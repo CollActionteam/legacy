3176f7f85d458fd6dc11324250a7d850d3b30a20
Luuk Boschker <luuk.boschker@gmail.com>
2018-08-12 14:18:34 +0200

    Add required validation for rich text components on server side

--- CollAction/ValidationAttributes/RichTextRequiredAttribute.cs
+++ CollAction/ValidationAttributes/RichTextRequiredAttribute.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace CollAction.ValidationAttributes
 {
-    public class RichTextRequiredAttribute : ValidationAttribute, IClientModelValidator
-    {
+    public class RichTextRequiredAttribute : RequiredAttribute, IClientModelValidator
+    {        
         public void AddValidation(ClientModelValidationContext context)
         {
+            var requiredMessage = ErrorMessage ?? $"{context.ModelMetadata.DisplayName} is required";
             context.Attributes["data-val"] = "true";
-            context.Attributes["data-val-richtextrequired"] = ErrorMessage ?? $"{context.ModelMetadata.DisplayName} is required";
+            context.Attributes["data-val-required"] = requiredMessage;
+            context.Attributes["data-val-richtextrequired"] = requiredMessage;
         }
     }
 }
