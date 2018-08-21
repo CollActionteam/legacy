e7a8355c32c2ed3db3911610b1d59c3462b436ca
dominicus <dominik.guz11@gmail.com>
2018-08-21 20:57:35 +0200

    EDIT: cleanup code

--- CollAction/Helpers/ProjectNormalizeStringExtensions.cs
+++ CollAction/Helpers/ProjectNormalizeStringExtensions.cs
@@ -9,13 +9,8 @@ namespace CollAction.Helpers
 {
     public static class ProjectNormalizeStringExtensions
     {
-        //Cyryllic to ASCII translit tables
-        static string[] lat_up = {"A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "Kh", "Ts", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya"};
-        static string[] lat_low = {"a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "\"", "y", "'", "e", "yu", "ya"};
-        static string[] rus_up = {"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
-        static string[] rus_low = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
-  
-        public static string ToUrlSlug(this string value){                      
+        public static string ToUrlSlug(this string value)
+        {                      
             //First to lower case
             value = value.ToLowerInvariant();
 
@@ -38,22 +33,6 @@ namespace CollAction.Helpers
 
             return value;
         }
-    
-        public static string RemoveAccent(this string txt) 
-        { 
-            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt); 
-            return System.Text.Encoding.ASCII.GetString(bytes); 
-        }
-
-        public static string Translit(this string str)
-        {
-            for (int i = 0; i <= 32; i++)
-            {
-                str = str.Replace(rus_up[i], lat_up[i]);
-                str = str.Replace(rus_low[i], lat_low[i]);              
-            }
-            return str;
-        }
 
         public static string RemoveDiacriticsFromString(this string text) 
         {
@@ -75,7 +54,6 @@ namespace CollAction.Helpers
         public static string NormalizeUriPart(this string project)
         {
             return project
-                .Translit()
                 .RemoveDiacriticsFromString()
                 .ToUrlSlug();
         }
