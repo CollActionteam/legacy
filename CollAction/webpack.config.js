14746152edd9e023a237696ce846040e0851052c
dominicus <dominik.guz11@gmail.com>
2018-09-16 12:13:04 +0200

    EDIT: Remove separate scripts, use npm versions initial, upgrade node packages to latest, error fixes pending

--- CollAction/webpack.config.js
+++ CollAction/webpack.config.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack');
 
 function getPlugins() {
-  const productionPlugins = [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-    }),
-  ];
+  // const productionPlugins = [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     minimize: true,
+  //   }),
+  // ];
 
   const compatibilityPlugins = [
     new webpack.ProvidePlugin({
@@ -13,20 +13,25 @@ function getPlugins() {
     }),
   ];
 
-  return compatibilityPlugins.concat(process.env.NODE_ENV === "production" ? productionPlugins : []);
+  return compatibilityPlugins; //.concat(process.env.NODE_ENV === "production" ? productionPlugins : [])
 }
 
 module.exports = {
-  entry: [ 'whatwg-fetch', 'quill', './Frontend/app/index.ts' ],
+  entry: [
+    'font-awesome/scss/font-awesome.scss',
+    'whatwg-fetch', 
+    'quill', 
+    './Frontend/app/index.ts' 
+  ],
   output: {
-    path: './wwwroot/js',
+    path: __dirname + '/wwwroot/js',
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.tsx?$/, loader: 'ts-loader' },
       {
         test: /\.scss$/,
@@ -35,6 +40,20 @@ module.exports = {
       {
         test: /\.css$/,
         loaders: ["style-loader", "css-loader?-url"],
+      },
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "url-loader?limit=10000&mimetype=application/font-woff" 
+      },
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        use: [{
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },
