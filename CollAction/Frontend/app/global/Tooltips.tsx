035407727c90fbb505f0f2466616065f96089047
Luuk Boschker <luuk.boschker@gmail.com>
2018-10-20 14:03:27 +0200

    Add goal tooltip

--- CollAction/Frontend/app/global/Tooltips.tsx
+++ CollAction/Frontend/app/global/Tooltips.tsx
@@ -49,3 +49,29 @@ renderComponentIf(
     </Tooltip>,
     document.getElementById("proposal-tooltip")
 );
+
+renderComponentIf(
+    <Tooltip
+        placement="topLeft"
+        align={{
+            offset: [-25, -10]
+        }}
+        arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+        overlay={
+            <span>
+                What is the problem that<br/>
+                you're trying to solve?<br/>
+                What is the impact if your<br/>
+                target is reached and we<br/>
+                collectively take the<br/>
+                proposed action? It’s especially<br/>
+                powerful if you can translate<br/>
+                it into numbers (e.g. X tons<br/>
+                of CO2 saved, Y lives<br/>
+                saved/improved, or Z <br/>
+                kilograms of plastic recycled).
+            </span>}>
+        <i className="fa fa-question-circle-o"></i>
+    </Tooltip>,
+    document.getElementById("goal-tooltip")
+);
