07193801ab95067338ec0e56cdc09f21d1dbc95f
dominicus <dominik.guz11@gmail.com>
2018-10-21 13:44:33 +0200

    EDIT: component for map marker

---
+++ CollAction/Frontend/app/global/MapMarkerItem.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import renderComponentIf from "./renderComponentIf";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faMapMarkerAlt);
+
+class MapMarkerItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="">
+        <FontAwesomeIcon icon={faMapMarkerAlt} />
+      </div>
+    );
+  }
+}
+
+renderComponentIf(
+  <MapMarkerItem />,
+  document.getElementById("icon-map-marker")
+);
