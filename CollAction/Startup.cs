4a75150e363d1ccf51a04c749d1f3388e61d3e20
logophobia <logophobia@users.noreply.github.com>
2018-10-23 20:39:03 +0100

    No app insights

--- CollAction/Startup.cs
+++ CollAction/Startup.cs
@@ -92,8 +92,6 @@ namespace CollAction
             services.AddDataProtection()
                     .Services.Configure<KeyManagementOptions>(options => options.XmlRepository = new DataProtectionRepository(new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(connectionString).Options));
 
-            services.AddApplicationInsightsTelemetry(Configuration);
-
             services.Configure<AuthMessageSenderOptions>(Configuration);
             services.Configure<ImageServiceOptions>(Configuration);
             services.Configure<NewsletterSubscriptionServiceOptions>(Configuration);
