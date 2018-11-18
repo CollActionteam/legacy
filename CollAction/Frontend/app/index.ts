1d2274985ef06aa69f3219eb8ba1a30020542e96
dominicus <dominik.guz11@gmail.com>
2018-11-18 13:13:10 +0100

    EDIT: Restore bluebird

--- CollAction/Frontend/app/index.ts
+++ CollAction/Frontend/app/index.ts
@@ -6,8 +6,13 @@ import "./admin";
 import "./manage";
 
 import "whatwg-fetch";
-
+import * as Promise from "bluebird";
 import * as jQuery from "jquery";
+
+Promise.config({
+  longStackTraces: true,
+  warnings: true
+});
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
@@ -16,11 +21,9 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { dom, library } from "@fortawesome/fontawesome-svg-core";
 
-// after library.add
 library.add(faTwitter, faFacebook, faLinkedin);
 dom.i2svg();
 
-// Let's Load jQuery in the window for the asp validations
 window["jQuery"] = jQuery;
 
 function displayBodyOnLoad() {
