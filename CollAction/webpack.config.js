4ea8a19eb0f13c179491510e961c0b7e62c959e4
dominicus <dominik.guz11@gmail.com>
2018-11-18 13:00:01 +0100

    EDIT: restore footer - bad merge

--- CollAction/webpack.config.js
+++ CollAction/webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-<<<<<<< HEAD
 const path = require('path')
 
 module.exports = {
@@ -11,73 +10,16 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, './wwwroot/js'),
-=======
-
-function getPlugins() {
-  // const productionPlugins = [
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     minimize: true,
-  //   }),
-  // ];
-
-  const compatibilityPlugins = [
-    new webpack.ProvidePlugin({
-      'Promise': 'bluebird', // Provide a Promise polyfill for older or less suportive browsers like IE9-11.
-    }),
-  ];
-
-  return compatibilityPlugins; //.concat(process.env.NODE_ENV === "production" ? productionPlugins : [])
-}
-
-function getMode() {
-  return process.env.NODE_ENV === "production" ? "production" : "development"
-}
-
-module.exports = {
-  mode: getMode(),
-  entry: [
-    'whatwg-fetch',
-    "font-awesome/scss/font-awesome.scss",
-    'quill', 
-    './Frontend/app/index.ts' 
-  ],
-  output: {
-    path: __dirname + '/wwwroot/js',
-    filename: 'bundle.js',
->>>>>>> 1474615... EDIT: Remove separate scripts, use npm versions initial, upgrade node packages to latest, error fixes pending
   },
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
     rules: [
-<<<<<<< HEAD
       { test: /\.tsx?$/, use: 'ts-loader' },      
       { test: /\.scss$/, use: ["style-loader", "css-loader?-url", "sass-loader"] },
       { test: /\.css$/, use: ["style-loader", "css-loader?-url"] },
       { test: /\.svg$/, use: 'svg-inline-loader' }
-=======
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader?-url", "sass-loader"],
-      },
-      {
-        test: /\.css$/,
-        loaders: ["style-loader", "css-loader?-url"],
-      },
-      {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-              loader: 'file-loader',
-              options: {
-                name: 'fonts/[name].[ext]',
-                mimetype: 'application/font-woff',
-                publicPath: '../'
-              }
-          }]
-      }
->>>>>>> 1474615... EDIT: Remove separate scripts, use npm versions initial, upgrade node packages to latest, error fixes pending
     ]
   },
   plugins: [
