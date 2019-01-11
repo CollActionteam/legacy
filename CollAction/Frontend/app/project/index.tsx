5d3627962ea8330f6eaa614012e4adf24036d931
lucgeurts <42180804+lucgeurts@users.noreply.github.com>
2019-01-11 16:57:48 +0100

    Removed unused css
    Remove file SimpleProjectCreate.scss and import in index.tsx. This contained css styles that were not in use anymore.

--- CollAction/Frontend/app/project/index.tsx
+++ CollAction/Frontend/app/project/index.tsx
@@ -4,7 +4,6 @@ import FindProject from "./FindProject";
 import "./styles/FindProject.scss";
 import "./styles/ProjectDetails.scss";
 import "./styles/StartInfo.scss";
-import "./styles/SimpleProjectCreate.scss";
 import "./styles/ProjectCreate.scss";
 import "./styles/ProjectCommit.scss";
 import "./styles/ProjectThankYouCommit.scss";
