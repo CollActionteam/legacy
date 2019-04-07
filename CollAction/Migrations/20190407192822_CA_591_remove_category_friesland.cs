4bc0c488ea1786a061e54936d4a8135c503c7443
dominicus <dominik.guz11@gmail.com>
2019-04-07 21:35:28 +0200

    EDIT: Add migration to remove friesland filter CA_591

---
+++ CollAction/Migrations/20190407192822_CA_591_remove_category_friesland.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CollAction.Migrations
+{
+    public partial class CA_591_remove_category_friesland : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Categories\" WHERE \"Name\" = 'Friesland'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Categories\" WHERE \"Name\" = 'Friesland'");
+        }
+    }
+}
