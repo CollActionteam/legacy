1ca6040af5db30455e95f4ee2615ddac7bf04f73
Luuk Boschker <luuk.boschker@gmail.com>
2019-04-13 14:55:39 +0200

    Use FAQ component on about page

--- CollAction/Frontend/app/global/Faqs.tsx
+++ CollAction/Frontend/app/global/Faqs.tsx
@@ -16,7 +16,7 @@ export default class Faq extends React.Component<IFaqProps> {
     renderTitle() {
         return (
             <h3>
-                <a href="#">{this.props.title}</a>
+                <a href="#" className="faq-title">{this.props.title}</a>
                 <span className="toggle">
                     <i className="fa fa-plus"></i>
                     <i className="fa fa-minus"></i>
@@ -35,6 +35,178 @@ export default class Faq extends React.Component<IFaqProps> {
         );
     }
 }
+
+renderComponentIf(
+    <Faq
+        title="What is CollAction?"
+        content={
+            <span>
+                <p>
+                    CollAction is a not-for-profit organization based in the Netherlands that
+                    helps people to solve Collective Action Problems through crowdacting.
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-what-is-collaction")
+);
+
+renderComponentIf(
+    <Faq
+        title="Huh? What is crowdacting?"
+        content={
+            <span>
+                <p>
+                    The term Crowdacting, as it is used here, is a term that has been introduced by CollAction. Basically, it means: ‘taking action collectively and conditionally with the purpose of achieving positive impact on the world and its inhabitants’.
+                    Simply put, with crowdacting we ask: ‘Would you take action if you knew that a hundred/a thousand/a million/a billion others would do so too?’ An example of a crowdacting project: ‘If we can find 10,000 people that commit to switch to renewable energy if 9,999 others would do the same, we will all switch together’. As a result, we form a critical mass of people that has a huge impact on making this world a better place. The concept is very similar to crowdfunding, but people commit to actions, instead of money.
+                </p>
+                <p>
+                In practice, a crowdacting project follows three steps:
+                </p>
+                <ol>
+                    <li>The ProjectStarter launches a project on collaction.org. He/she proposes a certain action (for example switching to renewable energy), a target (minimum number of participants), and a deadline.</li>
+                    <li>ProjectSupporters commit to taking action, if the target is met within the deadline.</li>
+                    <li>If and when the target is met, everybody acts collectively.</li>
+                </ol>
+                <p>
+                    More information can be found on <a href="http://www.crowdacting.org/" target="_blank">www.crowdacting.org</a>.
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-what-is-crowdacting")
+);
+
+renderComponentIf(
+    <Faq
+        title="Crowdacting – is that a new thing?"
+        content={
+            <span>
+                <p>
+                    Although the term crowdacting in its current meaning is new, the underlying concept is not. There are plenty of other examples of collective actions that contain elements of crowdacting. Think for instance of boycotts, demonstrations, collective bargaining initiatives, or petition websites. The difference is the fact that crowdacting combines three elements: 1) It’s (explicitly) conditional (it only happens if a set target is met); 2) it’s for social and/or ecological good; and 3) the action goes beyond signing a petition.
+                    More information on how crowdacting resembles and differs from other initiatives can be found on <a href="http://www.crowdacting.org/" target="_blank">www.crowdacting.org</a>.
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-crowdacting-a-new-thing")
+);
+
+renderComponentIf(
+    <Faq
+        title="What are collective action problems?"
+        content={
+            <span>
+                <p>
+                    Collective action problems are situations where it is in the interest of the group to work together, but in which coordination is a challenge. Because of this, every individual has an incentive to act in their own interest. This <a href="https://www.youtube.com/watch?v=p3KlgxYhDbk" target="_blank">video</a> explains it clearly.
+                </p>
+                <p>
+                    A well-known example of a collective action problem is the tragedy of the commons.
+                    The commons is a communal meadow where cows of different farmers can graze. Every farmer wants to earn as much as possible against the lowest possible costs and therefore has the individual incentive to let as many of his cows graze on the communal grass as possible. However, other farmers will do the same and soon the meadow will be destroyed due to overgrazing. There is little point in doing something about this for each individual farmer – knowing (or fearing) that other farmers will try and use the largest possible share of the communal grass and the meadow will still be overgrazed. The farmer is better off adding some more cows, so he will, at least, benefit from it in the short term. However, in the long term, all the farmers and as a consequence the community will lose out, because the piece of land can be written off due to overgrazing.
+                </p>
+                <p>
+                    Examples of (serious and less serious) collective action problems are: global warming, traffic jams, overpopulation, dismal working conditions, keeping a student house clean, corruption, overfishing, etc.
+                </p>
+                <p>
+                    Traditionally, there are two solutions to collective action problems:
+                    <ol>
+                        <li>Privatisation: the commons can be divided between the farmers. Now the farmers benefit from taking good care of their own piece of land, so that it is of use to them in the long term.</li>
+                        <li>Regulation: Agreements can be made and enforced about the usage of the commons. It is, for example, possible to decide on a maximum number of cows per farmer.</li>
+                    </ol>
+                    However, these solutions are not applicable to all collective action problems. It is not so easy (or desirable) to privatise certain goods (how do you privatise the ozone layer? Or migrating fish populations?). Coming up with regulations and enforce these regulations to solve (cross border) problems has proven difficult - just think about the cumbersome process to do something about climate change on an international level.
+                </p>
+                <p>
+                    So, we need a new solution to old problems: crowdacting.
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-what-are-collactive-problems")
+);
+
+renderComponentIf(
+    <Faq
+        title="Where can I learn more about crowdacting?"
+        content={
+            <span>
+                <a href="http://www.crowdacting.org/" target="_blank">www.crowdacting.org</a>.
+            </span>
+        } />,
+    document.getElementById("faq-where-to-learn-more")
+);
+
+renderComponentIf(
+    <Faq
+        title="Can I start a project myself?"
+        content={
+            <span>
+                Yes! You can create a project <a href="/Projects/StartInfo" target="_blank">here</a>. Make sure to check if your idea meets the CollAction criteria below.
+            </span>
+        } />,
+    document.getElementById("faq-start-it-yourself")
+);
+
+renderComponentIf(
+    <Faq
+        title="How long does a project run for?"
+        content={
+            <span>
+                Some actions are a one-off – think of the switch to a fair bank or green energy. Other projects can have a longer duration, such as eating less meat, periodically visiting lonely elderly people or helping refugees with language lessons and their integration. In other words, the duration of an action differs from one project to the other.
+            </span>
+        } />,
+    document.getElementById("faq-how-long-does-it-run")
+);
+
+renderComponentIf(
+    <Faq
+        title="What happens when a target has not been reached?"
+        content={
+            <span>
+                If the target is not met by the deadline, no one needs to act. Crowdacting means: No cure, no action. However, if you are really inspired to take action anyway, we will of course applaud this.
+            </span>
+        } />,
+    document.getElementById("faq-target-not-reached")
+);
+
+renderComponentIf(
+    <Faq
+        title="How do I know that other people will really take action?"
+        content={
+            <span>
+                <p>
+                    You can never be 100% sure. In the first place, crowdacting is about trust. Trusting that people want to work together to create a better world. Trusting that people are attached to their personal ideals. And trusting that people will keep their promises. We hope to build a community of people that take their commitments seriously and try to create a culture in which agreement=agreement.  
+                </p>
+                <p>
+                    That being said, we will incorporate certain processes and tools to ensure that as many project supporters proceed to taking action. What these processes are depends on the type of project. For some projects (such as switching projects, for example to a fair bank or green energy) the action can already be started conditionally at the time of commitment. When the target is reached, supporters of the project will automatically be transferred to the fair bank or renewable energy provider. When it comes to local projects, social control will play a bigger role. Other projects might be purely based on trust. We will be testing different tools and approaches in the upcoming projects. We ask ProjectStarters to seriously consider this question  – the team of CollAction can help you think this through!
+                </p>                    
+            </span>
+        } />,
+    document.getElementById("faq-how-do-you-know")
+);
+
+renderComponentIf(
+    <Faq
+        title="When will the projects end exactly?"
+        content={
+            <span>
+                <p>
+                    The project ends at the end (23:59:59) of the day that is listed as "End Date" in the UTC (London) timezone.
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-when-will-it-end")
+);
+
+renderComponentIf(
+    <Faq
+        title="Where can I learn more about the CollAction organization?"
+        content={
+            <span>
+                <p>
+                    You can read more about our mission, strategy, and financials in <a href="https://drive.google.com/open?id=1syiUgEq-Or-GOfvBYY8k3fgTu5UZvLey">this document</a>.
+                    The document is in Dutch for now, since we are headquartered in the Netherlands and need to submit it to local authorities - but we're working on a translation - apologies!
+                    But if you paste it in Google Translate, you should be able to get the gist ☺. 
+                </p>
+            </span>
+        } />,
+    document.getElementById("faq-about-the-organization")
+);
 
 renderComponentIf(
     <Faq
