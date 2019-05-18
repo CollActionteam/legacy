569d9b8b0e97dc31b1e8107a586340690b257bda
Luuk Boschker <luuk.boschker@gmail.com>
2019-05-18 15:26:09 +0200

    Moved enum in it's own file

--- CollAction/Services/Project/IParticipantsService.cs
+++ CollAction/Services/Project/IParticipantsService.cs
@@ -4,22 +4,12 @@ using CollAction.Models;
 
 namespace CollAction.Services.Project
 {
-    public interface IParticipantsService
+  public interface IParticipantsService
     {
         Task<AddParticipantResult> AddAnonymousParticipant(int projectId, string email);
         Task<AddParticipantResult> AddLoggedInParticipant(int projectId, string userId);
         Task RefreshParticipantCountMaterializedView();
         Task<ProjectParticipant> GetParticipant(string userId, int projectId);
         Task<string> GenerateParticipantsDataExport(int projectId);
-    }
-
-    public enum AddParticipantScenario
-    {
-        LoggedInAndAdded = 0,
-        AnonymousCreatedAndAdded = 1,
-        AnonymousAlreadyRegisteredAndAdded = 2,
-        AnonymousNotRegisteredPresentAndAdded = 3,
-        AnonymousAlreadyRegisteredAndAlreadyParticipating = 4,
-        AnonymousNotRegisteredPresentAndAlreadyParticipating = 5
     }
 }
