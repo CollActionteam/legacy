5ee71af16aa1bce3ae2c67c0655890df553797d3
Tim Stokman <timstokman@gmail.com>
2019-05-25 13:34:14 +0200

    Simplify

--- CollAction/Models/EmailViewModels/ProjectCommitEmailViewModel.cs
+++ CollAction/Models/EmailViewModels/ProjectCommitEmailViewModel.cs
@@ -12,79 +12,34 @@ namespace CollAction.Models.EmailViewModels
         public string ProjectUrl { get; set; }
 
         public string StartLink
-        {
-            get
-            {
-                return $"{PublicAddress}/Start";
-            }
-        }
+            => $"{PublicAddress}/Start";
 
         public string FinishRegistrationLink
-        {
-            get
-            {
-                return $"{PublicAddress}/account/FinishRegistration?email={WebUtility.UrlEncode(Result.ParticipantEmail)}&code={WebUtility.UrlEncode(Result.PasswordResetToken)}";
-            }
-        }
+            => $"{PublicAddress}/account/FinishRegistration?email={WebUtility.UrlEncode(Result.ParticipantEmail)}&code={WebUtility.UrlEncode(Result.PasswordResetToken)}";
 
         public string FacebookLink
-        {
-            get
-            {
-                return $"https://www.facebook.com/sharer/sharer.php?u={ProjectUrl}";
-            }
-        }
+            => $"https://www.facebook.com/sharer/sharer.php?u={ProjectUrl}";
 
         public string LinkedinLink
-        {
-            get
-            {
-                return $"http://www.linkedin.com/shareArticle?mini=true&url={ProjectUrl}&title={WebUtility.UrlEncode(Project.Name)}";
-            }
-        }
+            => $"http://www.linkedin.com/shareArticle?mini=true&url={ProjectUrl}&title={WebUtility.UrlEncode(Project.Name)}";
 
         public string TwitterLink
-        {
-            get
-            {
-                return $"https://twitter.com/intent/tweet?text={WebUtility.UrlEncode(Project.Name)}&url={ProjectUrl}";
-            }
-        }
+            => $"https://twitter.com/intent/tweet?text={WebUtility.UrlEncode(Project.Name)}&url={ProjectUrl}";
 
         public bool ShowAlreadyParticipating
-        {
-            get
-            {
-                return Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAlreadyParticipating ||
-                       Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAlreadyParticipating;
-            }
-        }
+            => Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAlreadyParticipating ||
+               Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAlreadyParticipating;
 
         public bool ShowYouCanLogin
-        {
-            get
-            {
-                return Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAlreadyParticipating ||
-                       Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAdded;
-            }
-        }
+            => Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAlreadyParticipating ||
+               Result.Scenario == AddParticipantScenario.AnonymousAlreadyRegisteredAndAdded;
 
         public bool ShowCreateYourOwnProject
-        {
-            get
-            {
-                return Result.Scenario == AddParticipantScenario.LoggedInAndAdded;
-            }
-        }
+            => Result.Scenario == AddParticipantScenario.LoggedInAndAdded;
 
         public bool ShowCreateAccount
-        {
-            get
-            {
-                return Result.Scenario == AddParticipantScenario.AnonymousCreatedAndAdded ||
-                       Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAdded ||
-                       Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAlreadyParticipating;
-            }
-        }
+            => Result.Scenario == AddParticipantScenario.AnonymousCreatedAndAdded ||
+               Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAdded ||
+               Result.Scenario == AddParticipantScenario.AnonymousNotRegisteredPresentAndAlreadyParticipating;
     }
 }
