240b3a8ee73c15d319c10057d77785fbf37685b7
Luuk Boschker <luuk.boschker@gmail.com>
2019-06-07 12:41:56 +0200

    Service returns Unknown now...

--- CollAction.Tests/Integration/NewsletterSubscriptionTests.cs
+++ CollAction.Tests/Integration/NewsletterSubscriptionTests.cs
@@ -108,7 +108,7 @@ namespace CollAction.Tests.Integration
 
             await _service.DeleteListMemberAsync(_newsletterTestListId, email);
             NewsletterSubscriptionService.SubscriptionStatus status = await _service.GetListMemberStatusAsync(_newsletterTestListId, email);
-            Assert.AreEqual(NewsletterSubscriptionService.SubscriptionStatus.NotFound, status);
+            Assert.AreEqual(NewsletterSubscriptionService.SubscriptionStatus.Unknown, status);
         }
 
         private string GetTestEmail()
