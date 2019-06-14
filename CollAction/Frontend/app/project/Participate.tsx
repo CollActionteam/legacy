af636064ca21414f40d4d2ec7304e201cbabfa23
Tim Stokman <timstokman@gmail.com>
2019-06-14 02:30:02 +0200

    CA-675 - client-side e-mail validation

--- CollAction/Frontend/app/project/Participate.tsx
+++ CollAction/Frontend/app/project/Participate.tsx
@@ -1,91 +1,116 @@
 import * as React from "react";
-import Collapsible from "react-collapsible";
 
 import renderComponentIf from "../global/renderComponentIf";
 
 interface IParticipationProps {
-  formId: string;
-  email: string;
-  projectActive: boolean;
-  projectStatus: string;
-  commitUrl: string;
+    formId: string;
+    email: string;
+    projectActive: boolean;
+    projectStatus: string;
+    commitUrl: string;
 }
 
-export default class ParticipateInProject extends React.Component<IParticipationProps> {
-  constructor(props) {
-    super(props);
-  }
+interface IParticipationState {
+    error: string;
+    email: string;
+}
 
-  displayContactEmail = () => (
-    <div>
-      <p>By clicking ‘Take Part’ you’re signing up with { this.props.email }.</p>
-    </div>
-  )
-
-  askContactEmail = () => (
-    <div>
-      <p>Want to participate? Enter your e-mail address and click 'Take Part'!</p>
-      <input className="form-control" name="email" type="email" placeholder="Your e-mail address"></input>
-    </div>
-  )
-
-  submitForm = () => {
-    const form = document.getElementById(this.props.formId) as HTMLFormElement;
-    if (form) {
-      form.submit();
-    }
-  }
-
-  render() {
-    let caption = "Participate!";
-    if (this.props.projectStatus === "ComingSoon") {
-      caption = "Coming soon";
-    }
-    else if (this.props.projectStatus === "Closed") {
-      caption = "Project is finished";
+export default class ParticipateInProject extends React.Component<IParticipationProps, IParticipationState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            email: null
+        };
     }
 
-    if (!this.props.projectActive) {
-      return (
-        <span className="take-part-button disable-commit">{ caption }</span>
-      );
-    }
-
-    return (
-      <div>
-        <div className="participant-information">
-          { this.props.email
-            ? this.displayContactEmail()
-            : this.askContactEmail() }
+    displayContactEmail = () => (
+        <div>
+            <p>By clicking ‘Take Part’ you’re signing up with {this.props.email}.</p>
         </div>
-        <p>
-          <a href="javascript:void(0)" onClick={ () => this.submitForm() } className="take-part-button">Take part</a>
-        </p>
-      </div>
-    );
-  }
+    )
+
+    displayError = () => {
+        if (this.state.error !== null) {
+            return <div className="error">
+                <p>{this.state.error}</p>
+            </div>;
+        }
+    }
+
+    setEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ email: event.currentTarget.value });
+    }
+
+    askContactEmail = () => (
+        <div>
+            <p>Want to participate? Enter your e-mail address and click 'Take Part'!</p>
+            {this.displayError()}
+            <input className="form-control" name="email" type="email" placeholder="Your e-mail address" onChange={(event) => this.setEmail(event)}></input>
+        </div>
+    )
+
+    submitForm = () => {
+        if (this.state.email.match(/^\S+@\S+\.\S+$/) === null) {
+            this.setState({ error: "Invalid e-mail address" });
+            return;
+        }
+        const form = document.getElementById(this.props.formId) as HTMLFormElement;
+        if (form) {
+            form.submit();
+        }
+    }
+
+    render() {
+        let caption = "Participate!";
+        if (this.props.projectStatus === "ComingSoon") {
+            caption = "Coming soon";
+        }
+        else if (this.props.projectStatus === "Closed") {
+            caption = "Project is finished";
+        }
+
+        if (!this.props.projectActive) {
+            return (
+                <span className="take-part-button disable-commit">{caption}</span>
+            );
+        }
+
+        return (
+            <div>
+                <div className="participant-information">
+                    {this.props.email
+                        ? this.displayContactEmail()
+                        : this.askContactEmail()}
+                </div>
+                <p>
+                    <a href="javascript:void(0)" onClick={() => this.submitForm()} className="take-part-button">Take part</a>
+                </p>
+            </div>
+        );
+    }
 }
 
 const infoCard = document.getElementById("participate-in-projectinfocard");
 renderComponentIf(
     <ParticipateInProject
-      formId={infoCard && infoCard.dataset.formId }
-      email={infoCard && infoCard.dataset.email}
-      commitUrl={infoCard && infoCard.dataset.commitUrl}
-      projectActive={infoCard && infoCard.dataset.projectActive !== "False"}
-      projectStatus={infoCard && infoCard.dataset.projectStatus}
+        formId={infoCard && infoCard.dataset.formId}
+        email={infoCard && infoCard.dataset.email}
+        commitUrl={infoCard && infoCard.dataset.commitUrl}
+        projectActive={infoCard && infoCard.dataset.projectActive !== "False"}
+        projectStatus={infoCard && infoCard.dataset.projectStatus}
     ></ParticipateInProject>,
     infoCard
 );
 
 const details = document.getElementById("participate-in-projectdetails");
 renderComponentIf(
-  <ParticipateInProject
-    formId={details && details.dataset.formId }
-    email={details && details.dataset.email}
-    commitUrl={details && details.dataset.commitUrl}
-    projectActive={details && details.dataset.projectActive !== "False"}
-    projectStatus={details && details.dataset.projectStatus}
-  ></ParticipateInProject>,
-  details
+    <ParticipateInProject
+        formId={details && details.dataset.formId}
+        email={details && details.dataset.email}
+        commitUrl={details && details.dataset.commitUrl}
+        projectActive={details && details.dataset.projectActive !== "False"}
+        projectStatus={details && details.dataset.projectStatus}
+    ></ParticipateInProject>,
+    details
 );
