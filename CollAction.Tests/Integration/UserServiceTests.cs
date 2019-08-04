8adabbab6e5777a24e4e55b0881f76edc2f8368d
Tim Stokman <timstokman@gmail.com>
2019-08-04 02:31:44 +0200

    Tests

--- CollAction.Tests/Integration/UserServiceTests.cs
+++ CollAction.Tests/Integration/UserServiceTests.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CollAction.Services.Email;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
 
 namespace CollAction.Tests.Integration
 {
     [TestClass]
     [TestCategory("Integration")]
-    public class UserServiceTests // TODO
+    public class UserServiceTests : IntegrationTestBase // TODO
     {
+        [TestMethod]
+        public Task TestUserCreate()
+            => WithServiceProvider(
+                ConfigureReplacementServices,
+                async scope =>
+                {
+                });
+
+        private void ConfigureReplacementServices(ServiceCollection sc)
+        {
+            sc.AddTransient(s => new Mock<IEmailSender>().Object);
+        }
     }
 }
