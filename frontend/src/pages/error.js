70019a48fe17643562b26779983b839fd1b47009
Tim Stokman <timstokman@gmail.com>
2019-08-05 23:44:48 +0200

    Report errors to user

--- frontend/src/pages/error.js
+++ frontend/src/pages/error.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default () => (
-    <h2>An error has occured</h2>
-);
+export default () => {
+    let err = new URLSearchParams(window.location.search).get("error");
+    return <h2>An error has occured: {err}</h2>;
+};
