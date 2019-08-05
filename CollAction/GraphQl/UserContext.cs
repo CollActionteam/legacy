82ecfe3b232f9dbcb1d8ecca518678ecac643065
Tim Stokman <timstokman@gmail.com>
2019-08-05 23:34:27 +0200

    Make admin claim available

--- CollAction/GraphQl/UserContext.cs
+++ CollAction/GraphQl/UserContext.cs
@@ -1,9 +1,10 @@
 ﻿using CollAction.Data;
+using GraphQL.Authorization;
 using System.Security.Claims;
 
 namespace CollAction.GraphQl
 {
-    public class UserContext
+    public class UserContext : IProvideClaimsPrincipal
     {
         public ClaimsPrincipal User { get; set; }
 
