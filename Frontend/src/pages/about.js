d9740d23276a8c3df5e480a97a0ead27189540e8
Luuk Boschker <luuk.boschker@gmail.com>
2019-08-16 19:30:22 +0200

    Dynamic about page!

--- Frontend/src/pages/about.js
+++ Frontend/src/pages/about.js
@@ -1,9 +1,50 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-export default () => (
-  <Layout>
-    <h1>About us</h1>
-    <p>This will be an about us page.</p>
-  </Layout>
-);
+export default function About({ data }) {
+  const sections = data.allMarkdownRemark.edges.map(e => e.node);
+
+  const mission = sections.find(s => s.frontmatter.name === "mission");
+  const about = sections.find(s => s.frontmatter.name === "about");
+  const join = sections.find(s => s.frontmatter.name === "join");
+  const partners = sections.find(s => s.frontmatter.name === "partners");
+
+  return (
+    <Layout>
+      <div>
+        <div>
+          <h1>{ mission.frontmatter.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: mission.html }}></div>
+        </div>
+        <div>
+          <h1>{ about.frontmatter.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: about.html }}></div>
+        </div>
+        <div>
+          <h1>{ join.frontmatter.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: join.html }}></div>
+        </div>
+        <div>
+          <h1>{ partners.frontmatter.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: partners.html }}></div>
+        </div>
+      </div>
+    </Layout>  
+  );
+}
+export const pageQuery = graphql`
+  query AboutPageQuery {
+    allMarkdownRemark(filter: {frontmatter: {type: {eq: "about"}}}) {
+      edges {
+        node {
+          html
+          frontmatter {
+            name
+            title
+          }
+        }
+      }
+    }
+  }
+`
