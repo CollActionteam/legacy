da0f6e721b80ad85633875ed977beca43eb00d95
Luuk Boschker <luuk.boschker@gmail.com>
2019-08-31 12:47:43 +0200

    Add intro-text to homepage

---
+++ Frontend/src/pages/home/intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid, Container } from '@material-ui/core';
+
+import styles from './style.module.scss';
+
+export const Intro = ({ title, content }) => {
+  return (
+    <Container className={ styles.introContainer }>
+      <Grid container>
+        <Grid item xs={12}>
+          <h2>{title}</h2>
+        </Grid>
+        <Grid item xs={12}>
+          <p dangerouslySetInnerHTML={{ __html: content }}></p>
+        </Grid>
+      </Grid>
+    </Container>
+);
+};
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -2,53 +2,55 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 import { Banner } from "../components/Banner";
-import { Container } from "@material-ui/core";
 import { CallToAction } from "./home/call-to-action";
+import { Intro } from "./home/intro";
 
 export const query = graphql`
-query HomePageQuery {
-  site {
-    siteMetadata {
-      title
-    }
-  }
-  photos: allHomeYaml(filter: {name: {eq: "photos"}}) {
-    edges {
-      node {
-        bannertitle
-        bannerphoto
-        name
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        title
       }
     }
-  }
-  content: allMarkdownRemark(filter: {frontmatter: {type: {eq: "home"}}}) {
-    edges {
-      node {
-        html
-        frontmatter {
-          title
+    photos: allHomeYaml(filter: { name: { eq: "photos" } }) {
+      edges {
+        node {
+          bannertitle
+          bannerphoto
           name
         }
       }
     }
+    content: allMarkdownRemark(
+      filter: { frontmatter: { type: { eq: "home" } } }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            name
+          }
+        }
+      }
+    }
   }
-}`;
+`;
 
 export default ({ data }) => {
   const photos = data.photos.edges
     .map(e => e.node)
-    .find(n => n.name = "photos");
+    .find(n => (n.name = "photos"));
 
-  
-    
+  const sections = data.content.edges.map(e => e.node);
+  const intro = sections.find(s => s.frontmatter.name === "intro");
+
   return (
     <Layout>
-      <Banner photo={ photos.bannerphoto }>
-        <CallToAction title={ photos.bannertitle }></CallToAction>
+      <Banner photo={photos.bannerphoto}>
+        <CallToAction title={photos.bannertitle}></CallToAction>
       </Banner>
-      <Container>
-        <p>Hello peoples</p>
-      </Container>
+      <Intro title={intro.frontmatter.title} content={intro.html}></Intro> 
     </Layout>
   );
-}
+};
