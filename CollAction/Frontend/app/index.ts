1d7e7800b60d9eae399ae0fd3940f140447bc4e2
Luuk <luuk.boschker@gmail.com>
2019-09-24 20:30:49 +0200

    Revert "CA-748-IE11 aligning fix"

--- CollAction/Frontend/app/index.ts
+++ CollAction/Frontend/app/index.ts
@@ -6,6 +6,16 @@ import "./admin";
 import "./manage";
 import "./donation";
 
+import "whatwg-fetch";
+
+import * as Promise from "bluebird";
+
+// Configure
+Promise.config({
+    longStackTraces: true,
+    warnings: true // note, run node with --trace-warnings to see full stack traces for warnings
+});
+
 function displayBodyOnLoad() {
   document.getElementById("body").style.display = "block";
 }
