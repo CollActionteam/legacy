101905f5c9729a4599a99313cc92d8e6cf8dd44d
Arnout Jansen <arnoutjansen@gmail.com>
2019-10-06 22:47:00 +0200

    Fix Section props type

--- Frontend/src/components/Section/index.tsx
+++ Frontend/src/components/Section/index.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "./style.module.scss";
 import { Container } from "@material-ui/core";
 
-export default ({ children, color, title }) => (
+export default ({
+  children,
+  color,
+  title,
+}: {
+  children: any;
+  color?: string;
+  title?: string;
+}) => (
   <section className={color ? styles[color] : null}>
     <Container>
       {title ? <h3 className={styles.title}>{title}</h3> : null}
