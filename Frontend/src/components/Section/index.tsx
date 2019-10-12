8857bcadb5d10eb9043a6749a7d4c4a5de16dd96
Luuk Boschker <luuk.boschker@gmail.com>
2019-10-12 13:02:34 +0200

    Add extra features to section component for generic text content

--- Frontend/src/components/Section/index.tsx
+++ Frontend/src/components/Section/index.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import styles from "./style.module.scss";
 import { Container } from "@material-ui/core";
 
-export default ({
-  children,
-  color,
-  title,
-}: {
+interface ISectionProps {
   children: any;
+  indent?: boolean;
   color?: string;
   title?: string;
-}) => (
-  <section className={color ? styles[color] : null}>
-    <Container>
-      {title ? <h3 className={styles.title}>{title}</h3> : null}
-      {children}
-    </Container>
-  </section>
-);
+}
+
+export const Section = ({ children, indent, color, title }: ISectionProps) => {
+  // tslint:disable-next-line: prettier
+  const classes = `${color ? styles[color] : ""} ${indent ? styles.indent : ""}`;
+  return (
+    <section className={classes}>
+      <Container>
+        {title ? <h3 className={styles.title}>{title}</h3> : null}
+        {children}
+      </Container>
+    </section>
+  );
+};
