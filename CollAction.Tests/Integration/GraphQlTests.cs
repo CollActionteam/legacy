d1a40fd4f344a8035e5ef396308a1030c1eb7e13
Tim Stokman <timstokman@gmail.com>
2019-12-08 20:03:02 +0100

    CA-769

---
+++ CollAction.Tests/Integration/GraphQlTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollAction.Tests.Integration
+{
+    [TestClass]
+    [TestCategory("Integration")]
+    public class GraphQlTests : IntegrationTestBase
+    {
+        //TODO
+    }
+}
