02de080c16ea7fbf0b6c45e3d6e81c307b5ed9dd
Tim Stokman <timstokman@gmail.com>
2019-12-31 22:47:56 +0100

    Simplify test

--- CollAction.Tests/Integration/ImageEndpointTests.cs
+++ CollAction.Tests/Integration/ImageEndpointTests.cs
@@ -17,40 +17,30 @@ namespace CollAction.Tests.Integration
     [TestCategory("Integration")]
     public class ImageEndpointTests : IntegrationTestBase
     {
-        const string Image = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=";
+        private readonly byte[] testImage = new byte[] { 0x42, 0x4D, 0x1E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1A, 0x00, 0x00, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00, 0xFF, 0x00 };
 
         [TestMethod]
         public Task TestImageUpload()
             => WithTestServer(
                    async (scope, testServer) =>
                    {
-                       int? imageId = null;
+                       using (var content = new MultipartFormDataContent())
+                       using (var memoryStream = new MemoryStream(testImage))
+                       using (var streamContent = new StreamContent(memoryStream))
+                       using (var descriptionContent = new StringContent("My Description"))
+                       using (var client = testServer.CreateClient())
+                       {
+                           SeedOptions seedOptions = scope.ServiceProvider.GetRequiredService<IOptions<SeedOptions>>().Value;
+                           client.DefaultRequestHeaders.Add("Cookie", await GetAuthCookie(client, seedOptions));
+                           content.Add(streamContent, "Image", "test.png");
+                           content.Add(descriptionContent, "ImageDescription");
+                           using (var response = await client.PostAsync("/upload", content))
+                           {
+                               string body = await response.Content.ReadAsStringAsync();
+                               Assert.IsTrue(response.IsSuccessStatusCode, body);
+                               int imageId = int.Parse(body);
 
-                       try
-                       {
-                           byte[] imageBytes = Convert.FromBase64String(Image);
-                           using (var content = new MultipartFormDataContent())
-                           using (var memoryStream = new MemoryStream(imageBytes))
-                           using (var streamContent = new StreamContent(memoryStream))
-                           using (var descriptionContent = new StringContent("My Description"))
-                           using (var client = testServer.CreateClient())
-                           {
-                               SeedOptions seedOptions = scope.ServiceProvider.GetRequiredService<IOptions<SeedOptions>>().Value;
-                               client.DefaultRequestHeaders.Add("Cookie", await GetAuthCookie(client, seedOptions));
-                               content.Add(streamContent, "Image", "test.png");
-                               content.Add(descriptionContent, "ImageDescription");
-                               using (var response = await client.PostAsync("/upload", content))
-                               {
-                                   string body = await response.Content.ReadAsStringAsync();
-                                   Assert.IsTrue(response.IsSuccessStatusCode, body);
-                                   imageId = int.Parse(body);
-                               }
-                           }
-                       }
-                       finally
-                       {
-                           if (imageId != null)
-                           {
+                               // Cleanup
                                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                                var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
                                var image = await context.ImageFiles.FindAsync(imageId);
