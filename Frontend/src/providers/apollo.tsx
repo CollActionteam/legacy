a192b3845d365276279bc8fa0d9a1f1567f2add1
Tim Stokman <timstokman@gmail.com>
2020-03-09 00:12:55 +0100

    Set correct domain everywhere

--- Frontend/src/providers/apollo.tsx
+++ Frontend/src/providers/apollo.tsx
@@ -4,7 +4,8 @@ import ApolloClient from 'apollo-boost';
 
 export default ({ children } : any) => {
   const client = new ApolloClient({
-    uri: `https://test.collaction.org/graphql`,
+    uri: process.env.REACT_APP_BACKEND_URL,
+    credentials: 'include'
   });
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
