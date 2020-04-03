1382168975ac6fac92f5976fac9e43e2d7202880
Tim Stokman <timstokman@gmail.com>
2020-04-03 03:34:24 +0200

    Fix hook usage

--- Frontend/src/pages/Donation/DonationReturn.tsx
+++ Frontend/src/pages/Donation/DonationReturn.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import { useLocation, useHistory } from "react-router-dom";
+import { useLocation, useHistory, Redirect } from "react-router-dom";
 import React from "react";
 import { Alert } from "../../components/Alert/Alert";
 import Loader from "../../components/Loader/Loader";
@@ -8,7 +8,6 @@ const DonationReturnPage = () => {
     const searchParams = new URLSearchParams(useLocation().search);
     const source = searchParams.get('source');
     const clientSecret = searchParams.get('client_secret');
-    const history = useHistory();
 
     const { data, loading, error } = useQuery(
         IDEAL_PAYMENT_SUCCEEDED,
@@ -23,9 +22,9 @@ const DonationReturnPage = () => {
     if (error) {
         return <Alert type="error" text="Unable to find iDeal information" />;
     } else if (!loading && data.donation.hasIDealPaymentSucceeded) {
-        history.push('/donate/thankyou');
+        return <Redirect to="/donate/thankyou" />
     } else if (!loading && data.donation.hasIDealPaymentSucceeded === false) {
-        history.push('/donate');
+        return <Redirect to="/donate" />
     }
     return <Loader />;
 };
