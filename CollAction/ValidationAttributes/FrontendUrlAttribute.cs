653da52a82761d21d2aae7d509a862d54d6894d3
Tim Stokman <timstokman@gmail.com>
2020-04-03 22:52:13 +0200

    Fix integration tests

--- CollAction/ValidationAttributes/FrontendUrlAttribute.cs
+++ CollAction/ValidationAttributes/FrontendUrlAttribute.cs
@@ -10,6 +10,11 @@ namespace CollAction.ValidationAttributes
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            if (value == null)
+            {
+                return ValidationResult.Success; // null should be handled with [Required]
+            }
+
             Uri canonicalAddress = new Uri(validationContext.GetRequiredService<IOptions<SiteOptions>>().Value.CanonicalAddress);
             if (!(value is string givenAddress))
             {
