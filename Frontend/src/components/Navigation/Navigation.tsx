c8f80a33e9f9ada68896d3872cf90c4a1ccd6589
Tim Stokman <timstokman@gmail.com>
2020-04-11 16:21:53 +0200

    Navigation to function component so it doesn't need to directly access usercontext

--- Frontend/src/components/Navigation/Navigation.tsx
+++ Frontend/src/components/Navigation/Navigation.tsx
@@ -1,40 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Hidden } from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import { UserContext } from "../../providers/User";
+import { useUser } from "../../providers/User";
 import { Button } from "../../components/Button/Button";
 import { IUser } from "../../api/types";
-
 import styles from "./Navigation.module.scss";
 
 interface INavigationProps {
   items: { name: string, link: string }[];
 }
 
-interface INavigationState {
-  collapsed: boolean;
-}
+export default ({ items } : INavigationProps) => {
+  const [ collapsed, setCollapsed ] = useState(true);
+  const toggleNavigation = () => setCollapsed(!collapsed);
+  const user = useUser();
 
-export default class Navigation extends React.Component<
-  INavigationProps,
-  INavigationState
-> {
-  constructor(props: INavigationProps) {
-    super(props);
-    this.toggleNavigation = this.toggleNavigation.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavigation() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  renderWithUser(user: IUser) {
+  const renderWithUser = (user: IUser) => {
     const returnUrl = window.location.href;
     const logoutUrl = `${process.env.REACT_APP_BACKEND_URL}/account/logout`;
     return <React.Fragment>
@@ -54,56 +36,46 @@ export default class Navigation extends React.Component<
       </React.Fragment>;
   }
 
-  renderWithoutUser() {
+  const renderWithoutUser = () => {
     return <li className={styles.navigationItem}>
       <Link className={styles.navigationButton} to="/account/login">
         Login
       </Link>
     </li>;
-  }
+  };
 
-  render() {
-    return (
-      <UserContext.Consumer>
-        {(user) =>
-          <div className={this.state.collapsed ? styles.collapsed : ""}>
-            <nav className={styles.navigation}>
-              <ul className={styles.navigationList}>
-                {this.props.items.map((link) => (
-                  <li key={link.name} className={styles.navigationItem}>
-                    <Link className={styles.navigationLink} to={link.link}>
-                      {link.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <ul className={styles.navigationList}>
-                <li className={styles.navigationItem}>
-                  <Link className={styles.navigationButton} to="/donate">
-                    <FontAwesomeIcon icon="heart" />
-                    Donate
-                  </Link>
-                </li>
-                { user ? this.renderWithUser(user) : this.renderWithoutUser() }
-              </ul>
-            </nav>
-            <Hidden mdUp>
-              <button
-                className={styles.navigationToggle}
-                onClick={this.toggleNavigation}
-              >
-                {this.state.collapsed ? (
-                  <FontAwesomeIcon icon="bars" />
-                ) : (
-                  <FontAwesomeIcon icon="times" />
-                )}
-              </button>
-            </Hidden>
-          </div>
-        }
-      </UserContext.Consumer>
-    );
-  }
-}
-
-Navigation.contextType = UserContext;
+  return <div className={collapsed ? styles.collapsed : ""}>
+    <nav className={styles.navigation}>
+      <ul className={styles.navigationList}>
+        {items.map((link) => (
+          <li key={link.name} className={styles.navigationItem}>
+            <Link className={styles.navigationLink} to={link.link}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.navigationList}>
+        <li className={styles.navigationItem}>
+          <Link className={styles.navigationButton} to="/donate">
+            <FontAwesomeIcon icon="heart" />
+            Donate
+          </Link>
+        </li>
+        { user ? renderWithUser(user) : renderWithoutUser() }
+      </ul>
+    </nav>
+    <Hidden mdUp>
+      <button
+        className={styles.navigationToggle}
+        onClick={toggleNavigation}
+      >
+        {collapsed ? (
+          <FontAwesomeIcon icon="bars" />
+        ) : (
+          <FontAwesomeIcon icon="times" />
+        )}
+      </button>
+    </Hidden>
+  </div>;
+};
