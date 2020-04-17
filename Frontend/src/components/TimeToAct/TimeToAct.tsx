8f6a1cbbd26b0441ba654c9c2d5ea24036d024aa
Tim Stokman <timstokman@gmail.com>
2020-04-17 23:27:54 +0200

    Missing styling

--- Frontend/src/components/TimeToAct/TimeToAct.tsx
+++ Frontend/src/components/TimeToAct/TimeToAct.tsx
@@ -8,21 +8,21 @@ export default () => {
 
     return <React.Fragment>
         <section className={styles.timeToActStep}>
-            <h4>Idea</h4>
+            <h4 className={styles.timeToActTitle}>Idea</h4>
             <div className={styles.timeToActStepBody}>
                 Propose a collective action and set a target number of participants.
             </div>
             <GhostButton onClick={() => history.push("/projects/start")}>Start crowdaction</GhostButton>
         </section>
         <section className={styles.timeToActStep}>
-            <h4>Crowd</h4>
-                <div className={styles.timeToActStepBody}>
+            <h4 className={styles.timeToActTitle}>Crowd</h4>
+            <div className={styles.timeToActStepBody}>
                 People pledge to take action if the target is met before the deadline.
             </div>
             <GhostButton onClick={() => history.push("/projects/find")}>Find crowdaction</GhostButton>
         </section>
         <section className={styles.timeToActStep}>
-            <h4>Action</h4>
+            <h4 className={styles.timeToActTitle}>Action</h4>
             <div className={styles.timeToActStepBody}>
                 If enough people commit, we all act!
             </div>
