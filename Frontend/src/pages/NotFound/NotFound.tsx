dd8ce2499f7c31215a8c706196f84de38b7b11f8
Jeppe Bijker <j.bijker@webzites.nl>
2020-05-02 21:02:08 +0200

    Page title added using Helmet

--- Frontend/src/pages/NotFound/NotFound.tsx
+++ Frontend/src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {Grid} from "@material-ui/core";
 import {Section} from "../../components/Section/Section";
+import {Helmet} from "react-helmet";
 
 const NotFoundPage = () => <>
+    <Helmet>
+        <title>Page not found</title>
+        <meta name="description" content="Page not found"/>
+    </Helmet>
     <Grid container>
         <Section>
             <h1>Not found...</h1>
