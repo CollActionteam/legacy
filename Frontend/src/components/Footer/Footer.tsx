f38e7a625122fb442e806ad0f42bf432090132b5
Arnout Jansen <arnoutjansen@gmail.com>
2020-05-03 15:14:55 +0200

    Updated footer

--- Frontend/src/components/Footer/Footer.tsx
+++ Frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { siteData } from "../../api/site";
 
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import logo from "../../assets/svg/logo.svg";
 
 import SocialMedia from "../SocialMedia/SocialMedia";
 import NewsletterSignup from "../NewsletterSignup/NewsletterSignup";
@@ -15,43 +13,46 @@ import { useSettings } from "../../providers/SettingsProvider";
 
 export default () => {
   const { mailChimpNewsletterListId } = useSettings();
+  const currentYear = new Date().getFullYear();
+
   return <div className={styles.footer}>
     <Container>
-      <Link to="/">
-        <img alt="CollAction" className={styles.logo} src={logo}></img>
-      </Link>
       <Grid container>
-        <Grid item xs={12} md={4}>
+        {siteData.footerLinks.map((column: { title: string, links: any[] }) => (
+          <Grid item xs={3}>
+            <h5 className={styles.title}>{column.title}</h5>
+            <nav className={styles.navigation}>
+              <ul className={styles.navigationList}>
+                {column.links.map((link: any, index: number) => (
+                  <li key={index} className={styles.navigationItem}>
+                    <HashLink className={styles.navigationLink} to={link.link}>
+                      {link.name}
+                    </HashLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </Grid>
+        ))}
+        <Grid item xs={12} md={3}>
+          <h5 className={styles.title}>Contact</h5>
           <div className={styles.description}>
             Any questions, comments, or would you like to work together?
             Awesome! Email us at{" "}
             <a href="mailto:hello@collaction.org">
               hello@collaction.org
             </a>
-            .<br />
-            Would you like to get an occasional update on CollAction and
-            crowdacting? Sign up for our newsletter (to your right)! We will be
-            careful with your data, see our <Link to="/privacy-policy">Privacy Policy</Link>.
           </div>
-        </Grid>
-        <Grid item xs={12} sm={6} md={5}>
-          <nav className={styles.navigation}>
-            <ul className={styles.navigationList}>
-              {siteData.footerLinks.map((link: any, index: number) => (
-                <li key={index} className={styles.navigationItem}>
-                  <HashLink className={styles.navigationLink} to={link.link}>
-                    {link.name}
-                  </HashLink>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <h5 className={styles.title}>Follow us</h5>
-          <SocialMedia socialMedia={siteData.socialMedia} />
           <h5 className={styles.title}>Newsletter</h5>
           <NewsletterSignup mailchimpListId={mailChimpNewsletterListId} />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12}>
+          <div className={styles.copyright}>
+            © {currentYear} CollAction
+            <SocialMedia socialMedia={siteData.socialMedia} />
+          </div>
         </Grid>
       </Grid>
     </Container>
