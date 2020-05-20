0d2bcc4b39412a37f55ab05a1cabd951eadff9a8
Tim Stokman <timstokman@gmail.com>
2020-05-21 00:44:50 +0200

    Return image conversion errors from backend

--- CollAction/Controllers/ImageController.cs
+++ CollAction/Controllers/ImageController.cs
@@ -2,7 +2,10 @@
 using CollAction.Services.Image;
 using CollAction.ViewModels.Upload;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,18 +16,28 @@ namespace CollAction.Controllers
     public sealed class ImageController : Controller
     {
         private readonly IImageService imageService;
+        private readonly ILogger<ImageController> logger;
 
-        public ImageController(IImageService imageService)
+        public ImageController(IImageService imageService, ILogger<ImageController> logger)
         {
             this.imageService = imageService;
+            this.logger = logger;
         }
 
         [HttpPost]
         [Authorize]
         public async Task<IActionResult> UploadImage([FromForm] UploadImageViewModel uploadImage, CancellationToken token)
         {
-            ImageFile image = await imageService.UploadImage(uploadImage.Image, uploadImage.ImageDescription, uploadImage.ImageResizeThreshold, token).ConfigureAwait(false);
-            return Ok(image.Id);
+            try
+            {
+                ImageFile image = await imageService.UploadImage(uploadImage.Image, uploadImage.ImageDescription, uploadImage.ImageResizeThreshold, token).ConfigureAwait(false);
+                return Ok(image.Id);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error uploading image");
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
     }
 }
