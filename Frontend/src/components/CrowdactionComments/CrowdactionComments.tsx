eb62f30c2ec2af7b18541f371a43c6665c60b93f
Tim Stokman <timstokman@gmail.com>
2020-05-22 19:29:56 +0200

    Crowdaction tweak comment retrieval

--- Frontend/src/components/CrowdactionComments/CrowdactionComments.tsx
+++ Frontend/src/components/CrowdactionComments/CrowdactionComments.tsx
@@ -19,8 +19,7 @@ const DELETE_COMMENT = gql`
     crowdaction {
       deleteComment(commentId: $commentId)
     }
-  }
-`;
+  }`;
 
 const CREATE_COMMENT = gql`
   mutation($crowdactionId: ID!, $comment: String!) {
@@ -35,14 +34,13 @@ const CREATE_COMMENT = gql`
         }
       }
     }
-  }
-`;
+  }`;
 
 const GET_COMMENTS = gql`
-  query($crowdactionId: ID!, $take: Int!, $skip: Int!) {
+  query($crowdactionId: ID!, $take: Int!, $before: String!) {
     crowdaction(id: $crowdactionId) {
       id
-      comments(take: $take, skip: $skip, orderBy: { path: "commentedAt", descending: true }) @connection(key: "comment", filter: ["crowdactionId"]) {
+      comments(take: $take, where: [{ path: "id", comparison: lessThan, value: [$before]}], orderBy: [{ path: "id", descending: true}]) {
         id
         comment
         commentedAt
@@ -54,181 +52,206 @@ const GET_COMMENTS = gql`
     }
   }`;
 
-export default ({ id }: ICrowdactionCommentsProps) => {
-    const numCommentsPerFetch = 10;
-    const user = useUser();
-    const [ page, setPage ] = useState(0);
-    const { data, loading, fetchMore, error } = useQuery<any>(
-      GET_COMMENTS,
-      {
-        variables: {
-          crowdactionId: id,
-          take: numCommentsPerFetch,
-          skip: 0
-        }
-      }
-    );
-    const [ deleteComment ] = useMutation(
-      DELETE_COMMENT,
-      {
-        update: (cache: DataProxy, mutationResult: FetchResult) => {
-          // Remove the comment from the cache
-          if (mutationResult.data) {
-            const removedId: number = mutationResult.data.crowdaction.deleteComment;
-            for (var p = 0; p <= page; p++) {
-              const vars = {
-                crowdactionId: id,
-                take: numCommentsPerFetch,
-                skip: p * numCommentsPerFetch
-              };
-              const comments: any = cache.readQuery({
-                query: GET_COMMENTS,
-                variables: vars
-              });
-              const toRemove = 
-                comments.crowdaction
-                        .comments
-                        .findIndex((c: any) => c.id === removedId);
-              if (toRemove >= 0) {
-                const currentComments = comments.crowdaction.comments;
-                const filteredComments = currentComments.slice(0, toRemove)
-                                                        .concat(currentComments.slice(toRemove + 1));
-                cache.writeQuery({
-                  query: GET_COMMENTS,
-                  variables: vars,
-                  data: {
-                    crowdaction: {
-                      __typename: comments.crowdaction.__typename,
-                      id: id,
-                      comments: filteredComments
-                    }
-                  }
-                });
-                return;
-              }
-            }
-          }
-        }
-      });
-    const [ createComment ] = useMutation(CREATE_COMMENT,
-      {
-        update: (cache: DataProxy, mutationResult: FetchResult) => {
-          // Add the comment to the top of the comment list by putting it in the cache
-          if (mutationResult.data) {
-            const newComment = mutationResult.data.crowdaction.createComment;
-            const vars = { crowdactionId: id, take: numCommentsPerFetch, skip: 0 };
-            const currentComments: any = cache.readQuery({
-              query: GET_COMMENTS,
-              variables: vars
-            });
-            cache.writeQuery({
-              query: GET_COMMENTS,
-              variables: vars,
-              data: {
-                crowdaction: {
-                  __typename: currentComments.crowdaction.__typename,
-                  id: id,
-                  comments: [ newComment, ...currentComments.crowdaction.comments ],
-                }
-              }
-            });
-          }
-        }
-      });
-    const loadMore = () => {
-      setPage(page + 1);
-      fetchMore({
-        variables: {
-          skip: numCommentsPerFetch * (page + 1)
-        },
-        updateQuery: (previousResult, { fetchMoreResult }) => {
-          const newCrowdaction = fetchMoreResult.crowdaction;
-          const previousCrowdaction = previousResult.crowdaction;
+const numCommentsPerFetch = 20;
+const maxCursor = "2147483647";
 
-          return {
-            // Put the new comments at the end of the list and update `pageInfo`
-            // so we have the new `endCursor` and `hasNextPage` values
-            crowdaction: {
-              __typename: previousCrowdaction.__typename,
-              id: previousCrowdaction.id,
-              comments: [...previousCrowdaction.comments, ...newCrowdaction.comments],
-            }
-          };
-        }
-      })        
+// Remove the comment from the cache
+const updateCacheAfterDelete = (cache: DataProxy, mutationResult: FetchResult, cursors: string[], id: string) => {
+  if (mutationResult.data) {
+    const removedId: number = mutationResult.data.crowdaction.deleteComment;
+
+    // Find the cache segment to remove this from
+    const cursor = cursors.find((c, i) => 
+      removedId < parseInt(c) && 
+      (i + 1 === cursors.length || removedId >= parseInt(cursors[i + 1])));
+
+    const vars = {
+      crowdactionId: id,
+      take: numCommentsPerFetch,
+      before: cursor
     };
-
-    useEffect(() => {
-      if (error) {
-        console.error(error?.message);
-      }
-    }, [ error ]);
-
-    const validationSchema = Yup.object({
-      comment: Yup.string()
+    const comments: any = cache.readQuery({
+      query: GET_COMMENTS,
+      variables: vars
     });
-    
-    const onSubmit = async (values: any, { setSubmitting, resetForm }: any) => { 
-      await createComment({
-        variables: { comment: values.comment, crowdactionId: id }
-      });
-      resetForm({});
-      setSubmitting(false);
-    };
+    const toRemove = 
+      comments.crowdaction
+              .comments
+              .findIndex((c: any) => c.id === removedId);
 
-    return <>
-        <Grid container spacing={4}>
-          <Alert type="error" text={error?.message} />
-          {
-            user ?
-                <Grid item xs={12}>
-                  <Formik initialValues={{ comment: '' }} validationSchema={validationSchema} onSubmit={onSubmit}>
-                    {(formik) =>
-                      <Form>
-                        <Card>
-                          <CardContent>
-                            <RichTextEditorFormControl
-                              formik={formik}
-                              name="comment"
-                              label="Comment"
-                              hint="Comment about this crowdaction"
-                              fullWidth />
-                          </CardContent>
-                          <CardActions>
-                            <Button type="submit">Comment</Button>
-                          </CardActions>
-                        </Card>
-                      </Form>
+    if (toRemove >= 0) {
+      const currentComments = comments.crowdaction.comments;
+      const filteredComments = currentComments.slice(0, toRemove)
+                                              .concat(currentComments.slice(toRemove + 1));
+      cache.writeQuery({
+        query: GET_COMMENTS,
+        variables: vars,
+        data: {
+          crowdaction: {
+            __typename: comments.crowdaction.__typename,
+            id: id,
+            comments: filteredComments
+          }
+        }
+      });
+      return;
+    };
+  }
+}
+
+// Add the comment to the top of the comment list by putting it in the cache
+const updateCacheAfterCreate = (cache: DataProxy, mutationResult: FetchResult, id: string) => {
+  if (mutationResult.data) {
+    const newComment = mutationResult.data.crowdaction.createComment;
+    const vars = { crowdactionId: id, before: maxCursor, take: numCommentsPerFetch };
+    const currentComments: any = cache.readQuery({
+      query: GET_COMMENTS,
+      variables: vars
+    });
+    cache.writeQuery({
+      query: GET_COMMENTS,
+      variables: vars,
+      data: {
+        crowdaction: {
+          __typename: currentComments.crowdaction.__typename,
+          id: id,
+          comments: [ newComment, ...currentComments.crowdaction.comments ],
+        }
+      }
+    });
+  }
+}
+
+// Put the new comments at the end of the list
+const updateCacheAfterFetch = (previousResult: any, { fetchMoreResult }: any) => {
+  const newCrowdaction = fetchMoreResult.crowdaction;
+  const previousCrowdaction = previousResult.crowdaction;
+
+  return {
+    crowdaction: {
+      __typename: previousCrowdaction.__typename,
+      id: previousCrowdaction.id,
+      comments: [...previousCrowdaction.comments, ...newCrowdaction.comments],
+    }
+  };
+}
+
+export default ({ id }: ICrowdactionCommentsProps) => {
+  const user = useUser();
+  const [ commentChangeNum, setCommentChangeNum ] = useState(0); // The net count difference in comments, counting deletions and new comments
+  const [ cursors, setCursors ] = useState([maxCursor]);
+  const { data, loading, fetchMore, error } = useQuery<any>(
+    GET_COMMENTS,
+    {
+      variables: {
+        crowdactionId: id,
+        take: numCommentsPerFetch,
+        before: maxCursor // Use the ID as cursor so we can have a 'stable' comment pagination
+      }
+    }
+  );
+  const comments = data?.crowdaction.comments;
+  const lastId = 
+    comments ? comments[comments.length - 1].id.toString() :
+                maxCursor;
+  const [ deleteComment ] = useMutation(
+    DELETE_COMMENT,
+    {
+      update: (cache: DataProxy, mutationResult: FetchResult) => {
+        updateCacheAfterDelete(cache, mutationResult, cursors, id);
+        setCommentChangeNum(commentChangeNum - 1);
+      }
+    });
+  const [ createComment ] = useMutation(CREATE_COMMENT,
+    {
+      update: (cache: DataProxy, mutationResult: FetchResult) => {
+        updateCacheAfterCreate(cache, mutationResult, id);
+        setCommentChangeNum(commentChangeNum + 1);
+      }
+    });
+  const loadMore = () => {
+    fetchMore({
+      variables: {
+        before: lastId
+      },
+      updateQuery: (previousResult, options) => {
+        setCursors(cursors.concat([lastId]));
+        return updateCacheAfterFetch(previousResult, options);
+      }
+    })        
+  };
+
+  useEffect(() => {
+    if (error) {
+      console.error(error?.message);
+    }
+  }, [ error ]);
+
+  const validationSchema = Yup.object({
+    comment: Yup.string()
+  });
+  
+  const onSubmit = async (values: any, { setSubmitting, resetForm }: any) => { 
+    await createComment({
+      variables: { comment: values.comment, crowdactionId: id }
+    });
+    resetForm({});
+    setSubmitting(false);
+  };
+
+  return <>
+      <Grid container spacing={4}>
+        <Alert type="error" text={error?.message} />
+        {
+          user ?
+              <Grid item xs={12}>
+                <Formik initialValues={{ comment: '' }} validationSchema={validationSchema} onSubmit={onSubmit}>
+                  {(formik) =>
+                    <Form>
+                      <Card>
+                        <CardContent>
+                          <RichTextEditorFormControl
+                            formik={formik}
+                            name="comment"
+                            label="Comment"
+                            hint="Comment about this crowdaction"
+                            fullWidth />
+                        </CardContent>
+                        <CardActions>
+                          <Button type="submit">Comment</Button>
+                        </CardActions>
+                      </Card>
+                    </Form>
+                  }
+                </Formik>
+              </Grid>
+              : null
+        }
+        {
+          data?.crowdaction.comments?.map((comment: any) => {
+              const commentDate = new Date(comment.commentedAt);
+              return <Grid key={comment.id} item xs={12}>
+                <Card>
+                  <CardContent>
+                    <h4>{ comment.user.fullName }</h4>
+                    <span dangerouslySetInnerHTML={{ __html: comment.comment}} />
+                  </CardContent>
+                  <CardActions>
+                    <em>{ Formatter.date(commentDate) } { Formatter.time(commentDate) }</em>
+                    {
+                      user?.isAdmin ? <Button onClick={() => deleteComment({ variables: { commentId: comment.id }})}>Delete Comment</Button>
+                                    : null
                     }
-                  </Formik>
-                </Grid>
-                : null
-          }
-          {
-            data?.crowdaction?.comments?.map((comment: any) => {
-                const commentDate = new Date(comment.commentedAt);
-                return <Grid key={comment.id} item xs={12}>
-                  <Card>
-                    <CardContent>
-                      <h4>{ comment.user.fullName }</h4>
-                      <span dangerouslySetInnerHTML={{ __html: comment.comment}} />
-                    </CardContent>
-                    <CardActions>
-                      <em>{ Formatter.date(commentDate) } { Formatter.time(commentDate) }</em>
-                      {
-                        user?.isAdmin ? <Button onClick={() => deleteComment({ variables: { commentId: comment.id }})}>Delete Comment</Button>
-                                      : null
-                      }
-                    </CardActions>
-                  </Card>
-                </Grid>;
-              })
-          }
-          { loading ? <Grid item xs={12}><Loader /></Grid> : null }
-          { data?.crowdaction?.comments.length % numCommentsPerFetch === 0 ? 
-              <Grid item xs={12}><Button onClick={() => loadMore()}>Load More</Button></Grid> : 
-              null
-          }
-        </Grid>
-    </>;
+                  </CardActions>
+                </Card>
+              </Grid>;
+            })
+        }
+        { loading ? <Grid item xs={12}><Loader /></Grid> : null }
+        { (data?.crowdaction?.comments.length - commentChangeNum) % numCommentsPerFetch === 0 ? 
+            <Grid item xs={12}><Button onClick={() => loadMore()}>Load More</Button></Grid> : 
+            null
+        }
+      </Grid>
+  </>;
 }
