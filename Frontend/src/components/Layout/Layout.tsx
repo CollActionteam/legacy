f378478b56cf5538b2bf87083da41a3479c92a17
Arnout Jansen <arnoutjansen@gmail.com>
2020-05-22 15:22:00 +0200

    Implemented mobile header navigation.

--- Frontend/src/components/Layout/Layout.tsx
+++ Frontend/src/components/Layout/Layout.tsx
@@ -1,43 +1,47 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 // Styling
-import "normalize.css";
-import "./style.scss";
+import 'normalize.css';
+import './style.scss';
 
 // Layout components
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 // Material-UI
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 
 // FontAwesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { 
-    faTwitter, 
-    faFacebookF,
-    faInstagram, 
-    faYoutube, 
-    faLinkedinIn
-} from "@fortawesome/free-brands-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-    faTimes,
-    faBars,
-    faAngleDown,
-    faSpinner,
-    faClock,
-    faTools,
-    faExclamationCircle,
-    faTrash,
-    faEuroSign,
-    faPlus,
-    faMinus,
-    faShareAlt
-} from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope, faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
-import ConsentDialog from "../ConsentDialog/ConsentDialog";
-import { useLocation } from "react-router-dom";
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+  faYoutube,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faTimes,
+  faBars,
+  faAngleDown,
+  faSpinner,
+  faClock,
+  faTools,
+  faExclamationCircle,
+  faTrash,
+  faEuroSign,
+  faPlus,
+  faMinus,
+  faShareAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+  faHeart,
+  faUser,
+} from '@fortawesome/free-regular-svg-icons';
+import ConsentDialog from '../ConsentDialog/ConsentDialog';
+import { useLocation } from 'react-router-dom';
 
 library.add(
   faHeart,
@@ -64,31 +68,31 @@ library.add(
 
 export default ({ children }: any) => {
   const { pathname } = useLocation();
-  const needsLayout = !pathname.endsWith("/widget");
+  const needsLayout = !pathname.endsWith('/widget');
   if (needsLayout) {
-    return <>
-      <Helmet
-        defaultTitle="CollAction"
-        titleTemplate="%s — CollAction"
-        meta={[
-          { name: "description", content: "CollAction" },
-          { name: "keywords", content: "collaction" },
-        ]}
-      ></Helmet>
-      <Header />
-      <Grid container className="site-content">
-        <Grid key="cookie" item xs={12}>
-          <ConsentDialog />
+    return (
+      <>
+        <Helmet
+          defaultTitle="CollAction"
+          titleTemplate="%s — CollAction"
+          meta={[
+            { name: 'description', content: 'CollAction' },
+            { name: 'keywords', content: 'collaction' },
+          ]}
+        ></Helmet>
+        <Header />
+        <Grid container className="site-content">
+          <Grid key="cookie" item xs={12}>
+            <ConsentDialog />
+          </Grid>
+          <Grid item xs={12}>
+            {children}
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          {children}
-        </Grid>
-      </Grid>
-      <Footer />
-    </>;
+        <Footer />
+      </>
+    );
   } else {
-    return <>
-      { children}
-    </>;
+    return <>{children}</>;
   }
-}; 
+};
