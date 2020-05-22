dc4791150a486a4620458afb26531631438b2ea0
Luuk Boschker <luuk.boschker@gmail.com>
2020-05-22 17:42:00 +0200

    WIP: add basic terraform definition

---
+++ Terraform/ecs.tf
@@ -0,0 +1,51 @@
+resource "aws_ecs_cluster" "collaction" {
+  name = "collaction-v2"
+}
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "ca-app-task"
+  execution_role_arn       = ""
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  container_definitions    = <<EOF
+    [
+      {
+        "name": "collaction-api",
+        "image": "collaction/backend:v2.0.0-beta1",
+        "networkMode": "awsvpc",
+        "cpu": 256,
+        "memory": 512,
+        "portMappings": [
+          {
+            "containerPort": 80,
+            "hostPort": 80
+          }
+        ],
+        "logConfiguration": {
+            "logDriver": "awslogs",
+            "options": {
+              "awslogs-group": "/ecs/collaction-api",
+              "awslogs-region": "${var.aws_region}",
+              "awslogs-stream-prefix": "ecs"
+            }
+        }
+      }
+    ]
+    EOF
+  depends_on = [
+    aws_cloudwatch_log_group.collaction-api
+  ]
+}
+
+# Container output is logged to CloudWatch
+resource "aws_cloudwatch_log_group" "collaction-api" {
+  name              = "/ecs/collaction-api"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_stream" "collaction-api" {
+  name           = "collaction-api"
+  log_group_name = aws_cloudwatch_log_group.collaction-api.name
+}
---
+++ Terraform/vars.tf
@@ -0,0 +1,4 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "eu-central-1"
+}
