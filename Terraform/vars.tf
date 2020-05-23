6cd53f58abd2948c2ba2a5d5c164343de4ba7837
Luuk Boschker <luuk.boschker@gmail.com>
2020-05-23 15:58:03 +0200

    Add variable for environment to build

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "The AWS region things are created in"
   default     = "eu-central-1"
 }
+
+variable "Environment" {
+  description = "Name of the environment to manage"
+  default     = "dev"
+}
