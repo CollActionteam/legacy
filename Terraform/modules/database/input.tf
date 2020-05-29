ff1a02e27b563673daeb1c0a2e9d5a9ba180f620
Luuk Boschker <luuk.boschker@gmail.com>
2020-05-29 21:25:55 +0200

    Add backup settings to RDS

--- Terraform/modules/database/input.tf
+++ Terraform/modules/database/input.tf
@@ -4,10 +4,20 @@ variable "identifier" {
 
 variable "allocated_storage" {
   description = "Nr of GBs to allocate as storage for this database"
-  default = 20
+  default     = 20
 }
 
 variable "instance_class" {
   description = "The instance class for this database"
-  default = "db.t2.micro"
+  default     = "db.t2.micro"
+}
+
+variable "backup_retention_periode" {
+  description = "The days to retain backups for. Set to 0 for no backups"
+  default     = 7
+}
+
+variable "delete_protection" {
+  description = "If the DB instance should have deletion protection enabled."
+  default     = true
 }
--- Terraform/staging/rds.tf
+++ Terraform/staging/rds.tf
@@ -1,7 +1,9 @@
 module "rds" {
   source = "../modules/database"
 
-  identifier = "collaction-${var.environment}"
-  instance_class = var.rds_instance_class
-  allocated_storage = var.rds_allocated_storage
+  identifier               = "collaction-${var.environment}"
+  instance_class           = var.rds_instance_class
+  allocated_storage        = var.rds_allocated_storage
+  backup_retention_periode = 0
+  delete_protection        = false
 }
