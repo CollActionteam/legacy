4551ee91cbf2794d0c9f7b3a4f3badb3b37a316f
Luuk Boschker <luuk.boschker@gmail.com>
2020-05-30 19:59:09 +0200

    Add CNAME to netlify

---
+++ Terraform/common/route53.tf
@@ -0,0 +1,14 @@
+# Add hostname to the DNS 
+data "aws_route53_zone" "collaction" {
+  name = var.route53_zone_name
+}
+
+resource "aws_route53_record" "netlify" {
+  zone_id = data.aws_route53_zone.collaction.zone_id
+  name    = "staging.collaction.org"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    "staging-collaction.netlify.app"
+  ]
+}
