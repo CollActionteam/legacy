c5b1bb91a4bbfec1d1edd03e3c858bfec59d1ecc
Luuk Boschker <luuk.boschker@gmail.com>
2020-05-31 01:11:09 +0200

    Database no longer publicly available

--- Terraform/production/rds.tf
+++ Terraform/production/rds.tf
@@ -4,5 +4,4 @@ module "rds" {
   identifier          = "collaction-${var.environment}"
   instance_class      = var.rds_instance_class
   allocated_storage   = var.rds_allocated_storage
-  publicly_accessible = true
 }
