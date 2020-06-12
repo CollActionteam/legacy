ebfc3c03f7c4ffba428c38879c260bad2efe04f4
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-12 14:43:06 +0200

    Add testselect task to Fargate

---
+++ Terraform/test/ecs-service.testselect.tf
@@ -0,0 +1,32 @@
+# The service running the task
+resource "aws_ecs_service" "testselect" {
+  name            = "testselect-collaction"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.testselect.arn
+  desired_count   = 1
+  
+  capacity_provider_strategy {
+    capacity_provider = var.capacity_provider
+    weight            = 100
+  }
+
+  network_configuration {
+    security_groups = [
+      aws_security_group.inbound-ecs.id,  # Allow traffic into the container
+      aws_security_group.outbound-ecs.id, # Allow traffic out of the container
+    ]
+    subnets          = module.vpc.subnet_ids.ids
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.testselect.id
+    container_name   = "testselect"
+    container_port   = 5000
+  }
+
+  depends_on = [
+    data.aws_alb_listener.api-collaction,
+    aws_iam_role_policy_attachment.ecs_task_execution_role
+  ]
+}
--- Terraform/test/ssm.tf
+++ Terraform/test/ssm.tf
@@ -3,6 +3,7 @@ module "ssm" {
   environment = var.environment
 }
 
+# Database settings for the database task
 resource "aws_ssm_parameter" "Db" {
   name  = "/collaction/${var.environment}/Db"
   type  = "String"
@@ -24,4 +25,45 @@ resource "aws_ssm_parameter" "DbPassword" {
   name  = "/collaction/${var.environment}/DbPassword"
   type  = "SecureString"
   value = random_password.db_password.result
+}
+
+# Settings for the testselect task
+resource "aws_ssm_parameter" "AwsSecretAccessKey" {
+  name  = "/testselect/AwsSecretAccessKey"
+  type  = "SecureString"
+  value = "todo"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "AwsSecretAccessKeyID" {
+  name  = "/testselect/AwsSecretAccessKeyID"
+  type  = "SecureString"
+  value = "todo"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "Username" {
+  name  = "/testselect/Username"
+  type  = "SecureString"
+  value = "todo"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "Password" {
+  name  = "/testselect/Password"
+  type  = "SecureString"
+  value = "todo"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
