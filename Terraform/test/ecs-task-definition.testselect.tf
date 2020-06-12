c767d8cd43565c94167c69e43a7582e016ac4dde
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-12 14:52:43 +0200

    Don't overwrite task name

--- Terraform/test/ecs-task-definition.testselect.tf
+++ Terraform/test/ecs-task-definition.testselect.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_task_definition" "testselect" {
-  family                   = "api-${var.environment}-collaction-task"
+  family                   = "testselect-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
