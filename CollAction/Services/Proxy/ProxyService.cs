f40a98c22a943bf62992164920edd6327d7183c3
Tim Stokman <timstokman@gmail.com>
2020-06-14 22:08:00 +0200

    Add more cdns

--- CollAction/Services/Proxy/ProxyService.cs
+++ CollAction/Services/Proxy/ProxyService.cs
@@ -9,14 +9,32 @@ using System.Threading.Tasks;
 
 namespace CollAction.Services.Proxy
 {
+    /*
+     * We're using this as a simple (probably incomplete) privacy proxy, 
+     * to stop people from being directly seen/fingerprinted/tracked by facebook 
+     * when visiting our site when we need to display stuff from instagram/facebook
+     */
     public sealed class ProxyService : IProxyService
     {
         private readonly HttpClient proxyClient;
+        // As a complete list as I can get of facebook/instagram cdns
         private static readonly string[] AllowedHosts =
             new string[]
             {
                 "fbcdn.net",
-                "cdninstagram.com"
+                "cdninstagram.com",
+                "fbsbx.com",
+                "tfbnw.net",
+                "fb.me",
+                "facebook.com.edgesuite.net",
+                "facebook.com.edgekey.net",
+                "facebook.net.edgekey.net",
+                "facebook-web-clients.appspot.com",
+                "fbcdn-profile-a.akamaihd.net",
+                "fbsbx.com.online-metrix.net",
+                "instagramstatic-a.akamaihd.net",
+                "akamaihd.net.edgesuite.net",
+                "internet.org"
             };
 
         public ProxyService(HttpClient proxyClient)
