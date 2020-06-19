e56df2491b75fd6f2bf6683001b6a878314187a4
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-19 14:31:33 +0200

    Add redirect rule definitions

---
+++ Terraform/staging/redirect-slowfashionseason.tf
@@ -0,0 +1,9 @@
+module "slowfashionseason" {
+  source = "../modules/redirectrule"
+
+  alb               = data.aws_alb.api-collaction
+  alb_listener      = data.aws_alb_listener.api-collaction
+  route53_zone_name = "slowfashionseason.org."
+  domain            = "slowfashionseason.org"
+  redirect_to_path  = "/crowdactions/slow-fashion-season-2020/174"
+}
