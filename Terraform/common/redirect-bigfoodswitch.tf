67c451ddaf2e3d19240063f161857792df7e3ff7
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-19 17:35:20 +0200

    Add redirect rules to common section

---
+++ Terraform/common/redirect-bigfoodswitch.tf
@@ -0,0 +1,9 @@
+module "bigfoodswitch" {
+  source = "../modules/redirectrule"
+
+  alb               = aws_alb.api-collaction
+  alb_listener      = aws_alb_listener.api-collaction
+  route53_zone_name = "bigfoodswitch.org."
+  domain            = "bigfoodswitch.org"
+  redirect_to_path  = "/crowdactions/the-big-food-switch/201"
+}
