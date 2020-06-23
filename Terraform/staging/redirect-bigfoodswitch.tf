e94e539fbb1130f5a61d5ab5d3fb05bfa93e276e
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-23 18:01:27 +0200

    Moved to common infrastructure

--- Terraform/staging/redirect-bigfoodswitch.tf
+++
@@ -1,9 +0,0 @@
-module "bigfoodswitch" {
-  source = "../modules/redirectrule"
-
-  alb               = data.aws_alb.api-collaction
-  alb_listener      = data.aws_alb_listener.api-collaction
-  route53_zone_name = "bigfoodswitch.org."
-  domain            = "bigfoodswitch.org"
-  redirect_to_path  = "/crowdactions/the-big-food-switch/201"
-}
--- Terraform/staging/redirect-slowfashionseason.tf
+++
@@ -1,9 +0,0 @@
-module "slowfashionseason" {
-  source = "../modules/redirectrule"
-
-  alb               = data.aws_alb.api-collaction
-  alb_listener      = data.aws_alb_listener.api-collaction
-  route53_zone_name = "slowfashionseason.org."
-  domain            = "slowfashionseason.org"
-  redirect_to_path  = "/crowdactions/slow-fashion-season-2020/174"
-}
