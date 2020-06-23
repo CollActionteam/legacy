c0ae57273acb13342735c1d9f6a144eeadbaa67d
Luuk Boschker <luuk.boschker@gmail.com>
2020-06-23 18:14:37 +0200

    Needs www prefix

--- Terraform/common/redirect-bigfoodswitch.tf
+++ Terraform/common/redirect-bigfoodswitch.tf
@@ -4,6 +4,6 @@ module "bigfoodswitch" {
   alb               = aws_alb.api-collaction
   alb_listener      = aws_alb_listener.api-collaction
   route53_zone_name = "bigfoodswitch.org."
-  domain            = "bigfoodswitch.org"
+  domain            = "www.bigfoodswitch.org"
   redirect_to_path  = "/crowdactions/the-big-food-switch/201"
 }
--- Terraform/common/redirect-slowfashionseason.tf
+++ Terraform/common/redirect-slowfashionseason.tf
@@ -4,6 +4,6 @@ module "slowfashionseason" {
   alb               = aws_alb.api-collaction
   alb_listener      = aws_alb_listener.api-collaction
   route53_zone_name = "slowfashionseason.org."
-  domain            = "slowfashionseason.org"
+  domain            = "www.slowfashionseason.org"
   redirect_to_path  = "/crowdactions/slow-fashion-season-2020/174"
 }
