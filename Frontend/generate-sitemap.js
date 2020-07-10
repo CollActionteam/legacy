d2a02727a4525f6abc1c24160c3b13b98e7e3004
Tim Stokman <timstokman@gmail.com>
2020-07-11 00:31:03 +0200

    Generate it a bit more dynamically

--- Frontend/generate-sitemap.js
+++ Frontend/generate-sitemap.js
@@ -5,10 +5,12 @@
  * nodejs, and generates a sitemap.xml in the /public folder, which gets
  * deployed to our CDN.
  *
- * NodeJS doesn't have a xml-dom library (except through external dependencies),
- * since this is pretty simple, I'm generating the sitemap XML through querying
- * the GraphQL API of the production site. We should be deploying regularly, 
- * so this is fine.
+ * I'm generating the sitemap XML through querying the GraphQL API of the 
+ * production site, and then generating the XML from that. We should be 
+ * deploying regularly, so this is fine.
+ * 
+ * Unfortunately, nodejs doesn't have a build-in xml generator, since this
+ * is a pretty simple job, I'm just outputting the xml manually.
  */
 
 const https = require('https');
@@ -27,14 +29,17 @@ const query = `
   }
 `;
 
+const backend = process.env.REACT_APP_BACKEND_URL;
+const frontend = process.env.URL;
+
 process.stdout.write('<?xml version="1.0" encoding="UTF-8"?>');
 process.stdout.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">');
-process.stdout.write('<url><loc>https://collaction.org/</loc><changefreq>always</changefreq><priority>1.0</priority></url>');
-process.stdout.write('<url><loc>https://collaction.org/privacy-policy</loc><changefreq>monthly</changefreq><priority>0.1</priority></url>');
-process.stdout.write('<url><loc>https://collaction.org/about</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>');
-process.stdout.write('<url><loc>https://collaction.org/donate</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>');
-process.stdout.write('<url><loc>https://collaction.org/find</loc><changefreq>always</changefreq><priority>0.3</priority></url>');
-https.get(`https://api.collaction.org/graphql?query=${encodeURIComponent(query)}`, (res) => {
+process.stdout.write(`<url><loc>${frontend}</loc><changefreq>always</changefreq><priority>1.0</priority></url>`);
+process.stdout.write(`<url><loc>${frontend}/privacy-policy</loc><changefreq>monthly</changefreq><priority>0.1</priority></url>`);
+process.stdout.write(`<url><loc>${frontend}/about</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>`);
+process.stdout.write(`<url><loc>${frontend}/donate</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>`);
+process.stdout.write(`<url><loc>${frontend}/find</loc><changefreq>always</changefreq><priority>0.3</priority></url>`);
+https.get(`${backend}/graphql?query=${encodeURIComponent(query)}`, (res) => {
   if (res.statusCode !== 200) {
     console.error(`GraphQL Error: ${res.statusCode}`);
     process.exit(1);
@@ -55,7 +60,7 @@ https.get(`https://api.collaction.org/graphql?query=${encodeURIComponent(query)}
             const changeFreq = crowdaction.isClosed ? 'monthly' : 'hourly';
             const cardImage = crowdaction.cardImage;
             const imageMap = cardImage ? `<image:image><image:loc>${cardImage.url}</image:loc><image:caption>${cardImage.description}</image:caption><image:title>${cardImage.description}</image:title></image:image>` : '';
-            process.stdout.write(`<url><loc>https://collaction.org${crowdaction.url}</loc><changefreq>${changeFreq}</changefreq><priority>${priority}</priority>${imageMap}</url>`);
+            process.stdout.write(`<url><loc>${frontend}${crowdaction.url}</loc><changefreq>${changeFreq}</changefreq><priority>${priority}</priority>${imageMap}</url>`);
           });
           process.stdout.write('</urlset>');
         });
