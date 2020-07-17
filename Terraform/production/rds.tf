f1f0d460be26601e566e688af15f092175f7db09
Luuk Boschker <luuk.boschker@gmail.com>
2020-07-17 14:20:22 +0200

    Enable perf insights for prod again

--- Terraform/production/rds.tf
+++ Terraform/production/rds.tf
@@ -5,7 +5,7 @@ module "rds" {
   instance_class               = var.rds_instance_class
   allocated_storage            = var.rds_allocated_storage
   backup_retention_period      = 7
-  performance_insights_enabled = false
+  performance_insights_enabled = true
   multi_az                     = true
   route53_zone_name            = var.route53_zone_name
   rds_hostname                 = var.rds_hostname
