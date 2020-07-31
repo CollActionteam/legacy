852ab6473a3bd4bb38bcd0a5de143aee1e470d69
Luuk Boschker <luuk.boschker@gmail.com>
2020-07-31 11:13:23 +0200

    Use new share component

--- Frontend/src/pages/crowdactions/ThankYouCommit/ThankYouCommit.tsx
+++ Frontend/src/pages/crowdactions/ThankYouCommit/ThankYouCommit.tsx
@@ -9,6 +9,7 @@ import {Fragments} from "../../../api/fragments";
 import styles from "./ThankYouCommit.module.scss";
 import CrowdactionShare from "../../../components/CrowdactionShare/CrowdactionShare";
 import DonationLayout from "../../../components/DonationLayout/DonationLayout";
+import SocialMediaSharing from "../SocialMediaSharing/SocialMediaSharing";
 
 type TParams = {
     slug: string,
@@ -39,8 +40,7 @@ const ThankYouCommitPage = ({match}: RouteComponentProps<TParams>): any => {
                 </Hidden>
                 <Grid item sm={12} md={7}>
                     <h3>Now go make waves and share it!</h3>
-
-                    {crowdaction && <CrowdactionShare crowdaction={crowdaction} />}
+                    {crowdaction && <SocialMediaSharing crowdaction={crowdaction} />}
                     <br/>
                 </Grid>
             </Grid>
