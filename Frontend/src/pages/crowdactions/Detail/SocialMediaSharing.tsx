c3ea154a4545b075b3cdc55fbaadaf187e1934cb
Luuk Boschker <luuk.boschker@gmail.com>
2020-07-31 10:26:47 +0200

    Use interface definition

--- Frontend/src/pages/crowdactions/Detail/SocialMediaSharing.tsx
+++ Frontend/src/pages/crowdactions/Detail/SocialMediaSharing.tsx
@@ -4,40 +4,42 @@ import { ICrowdaction } from '../../../api/types';
 
 import styles from './SocialMediaSharing.module.scss';
 
-const SocialMediaSharing = ({ crowdaction }: any) => {
+interface ISocialMediaSharingProps {
+  crowdaction: ICrowdaction;
+}
 
-  const action = crowdaction as ICrowdaction;  
+const SocialMediaSharing = ({ crowdaction }: ISocialMediaSharingProps) => {
   const shareUrl = window.location.href;
 
   return (
     <div className={styles.shareButtons}>
-      <FacebookShareButton quote={`${action.name}: ${action.proposal}`} hashtag={action.tags.map(tag => `#${tag.tag.name}`).join(' ')} url={shareUrl}>
+      <FacebookShareButton quote={`${crowdaction.name}: ${crowdaction.proposal}`} hashtag={crowdaction.tags.map(tag => `#${tag.tag.name}`).join(' ')} url={shareUrl}>
         <FacebookIcon size={32} round />
       </FacebookShareButton>
       <FacebookShareCount url={shareUrl}>
         {count => count}
       </FacebookShareCount>
 
-      <TwitterShareButton url={shareUrl} title={action.name} hashtags={action.tags.map(tag => `#${tag.tag.name}`)}>
+      <TwitterShareButton url={shareUrl} title={crowdaction.name} hashtags={crowdaction.tags.map(tag => `#${tag.tag.name}`)}>
         <TwitterIcon size={32} round />
       </TwitterShareButton>
 
-      <LinkedinShareButton url={shareUrl} title={action.name} summary={action.proposal}>
+      <LinkedinShareButton url={shareUrl} title={crowdaction.name} summary={crowdaction.proposal}>
         <LinkedinIcon size={32} round />
       </LinkedinShareButton>
 
-      <TumblrShareButton url={shareUrl} title={action.name} caption={action.proposal} tags={action.tags.map(tag => tag.tag.name)}>
+      <TumblrShareButton url={shareUrl} title={crowdaction.name} caption={crowdaction.proposal} tags={crowdaction.tags.map(tag => tag.tag.name)}>
         <TumblrIcon size={32} round />        
       </TumblrShareButton>
       <TumblrShareCount url={shareUrl}>
         {count => count}
       </TumblrShareCount>
 
-      <EmailShareButton url={shareUrl} subject={action.name} body="Hi! Check out this crowdaction: ">
+      <EmailShareButton url={shareUrl} subject={crowdaction.name} body="Hi! Check out this crowdaction: ">
         <EmailIcon size={32} round />
       </EmailShareButton>
 
-      <WhatsappShareButton url={shareUrl} title={`Hi! Check out this crowdaction: ${action.name}`}>
+      <WhatsappShareButton url={shareUrl} title={`Hi! Check out this crowdaction: ${crowdaction.name}`}>
         <WhatsappIcon size={32} round />
       </WhatsappShareButton>
     </div>
