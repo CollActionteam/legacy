3665913cc05e4f40fdb9f3d78fcdfb970aa2cf18
Luuk Boschker <luuk.boschker@gmail.com>
2021-03-19 10:27:04 +0100

    Add discontinuation info on homepage

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -18,6 +18,19 @@ const HomePage = () => {
         <title>CollAction</title>
         <meta name="description" content="CollAction" />
       </Helmet>
+      <Section center color="green">
+      <p>
+          Due to the amazing success and traction of one of our crowdactions - <a href="https://slowfashion.global" target="_blank" rel="noopener noreferrer">The Slow Fashion Season</a> - 
+          we have decided to focus our limited resources on further growing the Slow Fashion Movement.
+        </p>
+        <p>
+          Therefore, Collaction.org is currently not actively managed. Although we wholeheartedly believe 
+          in the concept and mission, we simply don’t have the time to do both at the moment.           
+          That’s why we're looking for an organization or team of people that want to take the proven concept of CollAction.org to the next level! 
+          Please reach out to <a href="mailto:hello@collaction.org">hello@collaction.org</a> 
+          if you're interested and read more on our blog <a href="https://hellocollaction.medium.com/collaction-org-172731a422e6" target="_blank" rel="noopener noreferrer">here</a>.
+        </p>        
+      </Section>
       <Carousel title={t('home.carousel.title')} text={t('home.carousel.text')} />
       <Section center color="grey" title={t('home.intro.title')}>
         <p dangerouslySetInnerHTML={{ __html: t('home.intro.text') }} />
