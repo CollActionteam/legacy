c7518bd7fd9a88b8dbef079f89501d84ca77f56f
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2021-09-12 17:57:19 +0200

    Add meta tags for social media

--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -6,6 +6,22 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta property="og:title" content="CollAction" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Do you want to make the world a better place, but do you sometimes feel that your actions are only a drop in the ocean? Well, not anymore, because we are introducing crowdacting: taking action knowing that you are one of many. We revamp the neighbourhood with a hundred people or switch to renewable energy with thousands at the same time. Find out more about crowdacting, take action collectively with one of the existing crowdactions, or start your own crowdaction."
+          />
+          <meta
+            property="og:image"
+            content="/public/android-chrome-256x256.png"
+          />
+
+          <meta
+            name="description"
+            content="Do you want to make the world a better place, but do you sometimes feel that your actions are only a drop in the ocean? Well, not anymore, because we are introducing crowdacting: taking action knowing that you are one of many. We revamp the neighbourhood with a hundred people or switch to renewable energy with thousands at the same time. Find out more about crowdacting, take action collectively with one of the existing crowdactions, or start your own crowdaction."
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
