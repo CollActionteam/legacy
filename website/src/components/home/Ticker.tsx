4321cce453b53a638f9eeb5163fc63c9b3d6efbb
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2021-09-23 18:58:18 +0200

    Fix apostrophes

--- website/src/components/home/Ticker.tsx
+++ website/src/components/home/Ticker.tsx
@@ -28,23 +28,23 @@ export default function Ticker() {
     <section className="p-5 py-12 md:py-32">
       <p className="text-xl md:text-4xl text-center ticker-wrapper">
         <div className="ticker-size-guide">
-          Let's <span className="text-collaction">switch to a green energy provider.</span>
+          Let&apos;s <span className="text-collaction">switch to a green energy provider.</span>
         </div>
 
         <div className={clsx("ticker-item", activeSentences[0][0] && "active-ticker-item")}>
-          Let's <span className="text-collaction">be vegan for a month.</span>
+          Let&apos;s <span className="text-collaction">be vegan for a month.</span>
         </div>
         <div className={clsx("ticker-item", activeSentences[1][0] && "active-ticker-item")}>
-          Let's <span className="text-collaction">separate our waste.</span>
+          Let&apos;s <span className="text-collaction">separate our waste.</span>
         </div>
         <div className={clsx("ticker-item", activeSentences[2][0] && "active-ticker-item")}>
-          Let's <span className="text-collaction">reduce food waste.</span>
+          Let&apos;s <span className="text-collaction">reduce food waste.</span>
         </div>
         <div className={clsx("ticker-item", activeSentences[3][0] && "active-ticker-item")}>
-          Let's <span className="text-collaction">switch to a green energy provider.</span>
+          Let&apos;s <span className="text-collaction">switch to a green energy provider.</span>
         </div>
         <div className={clsx("ticker-item", activeSentences[4][0] && "active-ticker-item")}>
-          Let's <span className="text-collaction">meet with an elderly once a week.</span>
+          Let&apos;s <span className="text-collaction">meet with an elderly once a week.</span>
         </div>
         <div className={clsx("ticker-item", activeSentences[5][0] && "active-ticker-item")}>
           <span className="text-collaction font-medium">
